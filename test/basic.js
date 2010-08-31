818912126988526c7369dcd38a47a5f2805f195d
Tim-Smart <tim@fostle.com>
2010-09-01 00:41:41 +1200

    id3v2 initial implementation

--- test/basic.js
+++ test/basic.js
@@ -1,14 +1,18 @@
 var ID3 = require('../lib/id3'),
     fs  = require('fs');
 
-var file_3v1 = fs.readFileSync(__dirname + '/sample3v1'),
-    file_3v2 = fs.readFileSync(__dirname + '/sample3v2'),
-    file_4   = fs.readFileSync(__dirname + '/sample4');
+var file_3v1 = fs.readFileSync(__dirname + '/sample3v1.mp3'),
+    file_3v2 = fs.readFileSync(__dirname + '/sample3v2.mp3'),
+    file_4   = fs.readFileSync(__dirname + '/sample4.m4a');
 
 var id3_3v1 = new ID3(file_3v1),
     id3_3v2 = new ID3(file_3v2),
-    id3_4     = new ID3(file_4);
+    id3_4   = new ID3(file_4);
 
 console.log(id3_3v1.getTags());
-console.log(id3_3v2.getTags());
+
+// ID3v2
+id3_3v2.parse();
+console.log(id3_3v2.get('title'));
+
 console.log(id3_4.getTags());
