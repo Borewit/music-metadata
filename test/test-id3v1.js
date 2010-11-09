698b7ca2c5c6faeb1adfcaa3c2a4affabd65b07c
Lee Treveil <leetreveil@gmail.com>
2010-11-09 17:27:57 +0000

    id3v1 tests are no longer dependant on index.js

--- test/test-id3v1.js
+++ test/test-id3v1.js
@@ -2,34 +2,34 @@
       fs = require('fs');
 
 var id3v1 = new id3(fs.readFileSync('samples/id3v1.mp3'));
-id3v1.parse();
+var tags = id3v1.parse();
 
 exports.title = function(test){
-    test.equal(id3v1.get('title'),'Blood Sugar', 'title is not correct');
+    test.equal(tags.title, 'Blood Sugar', 'title is not correct');
     test.done();
 }
 
 exports.artist = function(test){
-    test.equal(id3v1.get('artist'),'Pendulum', 'artist is not correct');
+    test.equal(tags.artist, 'Pendulum', 'artist is not correct');
     test.done();
 }
 
 exports.album = function(test){
-    test.equal(id3v1.get('album'),'Blood Sugar (Single)', 'album is not correct');
+    test.equal(tags.album, 'Blood Sugar (Single)', 'album is not correct');
     test.done();
 }
 
 exports.comment = function(test){
-    test.equal(id3v1.get('comment'),'abcdefg', 'comment is not correct');
+    test.equal(tags.comment, 'abcdefg', 'comment is not correct');
     test.done();
 }
 
 exports.genre = function(test){
-    test.equal(id3v1.get('genre'),'Electronic', 'genre is not correct');
+    test.equal(tags.genre,'Electronic', 'genre is not correct');
     test.done();
 }
 
 exports.year = function(test){
-    test.equal(id3v1.get('year'),'2007', 'year is not correct');
+    test.equal(tags.year,'2007', 'year is not correct');
     test.done();
 }
