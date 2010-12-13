86e617f8431aa588643615c2a46ad1d83f5a46ff
Lee Treveil <leetreveil@gmail.com>
2010-12-13 23:33:47 +0000

    processors now emit their own 'done' event, this indicates to the user when parsing has finished. This can be useful if you want to destroy the stream as soon as possible (improves performance). The 'metadata' event is now emitted on the 'done' event and should return data faster.
    Only implemented on id4.js as of now.

--- test/asynctest.js
+++ test/asynctest.js
@@ -1,86 +1,23 @@
-var strtok = require('strtok'),
-    fs = require('fs'),
-    id4 = require('../lib/index'),
-    id3v2 = require('../lib/id3v2'),
-    ID3File = require('../lib/index'),
-    genres = require('../lib/common').GENRES;
+var fs = require('fs'),
+    midy = require('../lib/index');
         
+var stream = fs.createReadStream('./samples/id4.m4a');
+var tst = new midy(stream);
 
-var tst = new id4(require('fs').createReadStream('./samples/id4.m4a'));
+stream.on('data', function(result){
+    console.log(result.length);
+});
 
-// if('©' == '©'){
-    // console.log("haggis");
-// }
+tst.on('tmpo', function(result){
+    console.log(result);
+});
 
-// var copyright = new Buffer(2);
-// copyright[1] = 0xA9;
-
-// console.log(copyright.toString());
-
-//console.log('©'.charCodeAt());
+tst.on('done', function(result){
+    stream.destroy();
+});
 
 tst.on('metadata', function(result) {
     console.log(result);
 });
 
 tst.parse();
-
-//var id3z = new require('../lib/index.js').ID3File();
-
-
-// var testid3v23 = new id3v2(fs.createReadStream('./samples/id3v2.3.mp3'));
-
-// testid3v23.on('TALB', function(result){
-    // console.log(result);
-// });
-
-// testid3v23.parse();
-
-
-// testid3.on('covr', function(result){
-	// console.log(result);
-    // var output = fs.createWriteStream('myfile.jpg');
-    // output.write(result.data);
-    // output.end();
-// });
-// testid3.on('�alb', function(result){
-	// console.log(result);
-// });
-
-// testid3.on('�day', function(result){
-	// console.log(result);
-// });
-
-// testid3.on('trkn', function(result){
-	// console.log(result);
-// });
-
-// testid3.on('gnre', function(result){
-	// console.log(result);
-// });
-
-// testid3.on('disk', function(result){
-	// console.log(result);
-// });
-
-// testid3.on('�nam', function(result){
-    // console.log('faggle');
-	// console.log(result);
-// });
-
-// testid3.on('�cmt', function(result){
-	// console.log(result);
-// });
-
-// testid3.on('©wrt', function(result){
-    // console.log('wuuuurt');
-	// console.log(result);
-// });
-
-// testid3.on('aART', function(result){
-	// console.log(result);
-// });
-
-// testid3.on('�ART', function(result){
-	// console.log(result);
-// });
