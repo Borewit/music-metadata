6cdcff9fe70cc8cfd879499e343519ff140e2b53
Lee Treveil <leetreveil@gmail.com>
2011-04-19 23:23:46 +0100

    Fixed bug with STDMETADATA being in the wrong scope. Fixed & unit-tested.

---
+++ test/test-metadataleak.js
@@ -0,0 +1,17 @@
+var fs = require('fs'),
+    mm = require('../lib/index'),
+    assert = require('assert'),
+    testHelper = require('./testHelper');
+    
+var testHelper = new testHelper(3, __filename);
+
+for (var i=0; i < 3; i++) {
+  var sample = require('path').join(__dirname, 'samples/id3v1.mp3');
+  var parser = new mm(fs.createReadStream(sample));
+  
+  parser.on('metadata', function(result) {
+    //we are testing that the metadata object is not being shared across parsers
+    assert.deepEqual(result.artist, ['Pendulum']);
+    testHelper.ranTests(1);
+  });
+}
