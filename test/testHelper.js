7f7e8825233e45c7f4180d4b8e6d657899da2eb1
Lee Treveil <leetreveil@gmail.com>
2011-05-03 18:05:21 +0100

    Bumped version to 0.1.1

--- test/testHelper.js
+++ test/testHelper.js
@@ -13,13 +13,18 @@ TestHelper.prototype.ranTests = function(testsRan) {
   
   var self = this;
   this.timeout = setTimeout(function() {
-    //report results
-    function color(text) {
-      return (self.testsRan === self.expected) ? '\033[32m' + text + '\033[0m' //green
-                                               : '\033[31m' + text + '\033[0m'; //red
-    }
-
-    console.log('%s ran %s out of %s tests', 
-      path.basename(self.fileName), color(self.testsRan), color(self.expected));
+    self.report();
   }, 500);
+}
+
+TestHelper.prototype.report = function() {
+  var self = this;
+  function color(text) {
+    return (self.testsRan === self.expected) ? '\033[32m' + text + '\033[0m' //green
+                                             : '\033[31m' + text + '\033[0m'; //red
+  }
+  
+  //report results
+  console.log('%s ran %s out of %s tests', path.basename(self.fileName), 
+    color(self.testsRan), color(self.expected));
 }
