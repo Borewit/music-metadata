d6f4ca8a38743f6dea973c5a9f00f6b879dd29d1
Lee Treveil <leetreveil@gmail.com>
2011-05-03 12:32:39 +0100

    Now works with npm test / other refactorings

--- test/alltests.js
+++ test/alltests.js
@@ -1,6 +1,6 @@
 if (module == require.main) {
   var spawn = require('child_process').spawn,
-        path = require('path');
+      path = require('path');
     
   var tests = ['test-genres.js', 'test-deunsync.js', 'test-metadataleak.js',
                'test-id3v1.1.js', 'test-id3v2.2.js', 'test-id3v2.3.js', 
@@ -10,13 +10,7 @@ if (module == require.main) {
   for (var i=0; i < tests.length; i++) {
     var fullPath = path.join(__dirname, tests[i]);
     var test = spawn(process.execPath, [fullPath]);
-    
-    test.stdout.on('data', function(data) {
-      console.log(data.toString());
-    });
-    
-    test.stderr.on('data', function(data) {
-      console.log(data.toString());
-    }); 
+    test.stdout.pipe(process.stdout);
+    test.stderr.pipe(process.stderr);
   }
 }
--- test/testHelper.js
+++ test/testHelper.js
@@ -13,20 +13,13 @@ TestHelper.prototype.ranTests = function(testsRan) {
   
   var self = this;
   this.timeout = setTimeout(function() {
-    self.report();
-  }, 500);
-}
+    //report results
+    function color(text) {
+      return (self.testsRan === self.expected) ? '\033[32m' + text + '\033[0m' //green
+                                               : '\033[31m' + text + '\033[0m'; //red
+    }
 
-TestHelper.prototype.report = function() {
-  function red(str) {
-    return '\033[31m' + str + '\033[0m';
-  }
-  function green(str) {
-    return '\033[32m' + str + '\033[0m';
-  }
-  if (this.testsRan !== this.expected) {
-    console.log(path.basename(this.fileName) + ' ran ' + red(this.testsRan) + ' out of ' + red(this.expected) + ' tests');
-  } else {
-    console.log(path.basename(this.fileName) + ' ran ' + green(this.testsRan) + ' out of ' + green(this.expected) + ' tests');
-  }
+    console.log('%s ran %s out of %s tests', 
+      path.basename(self.fileName), color(self.testsRan), color(self.expected));
+  }, 500);
 }
