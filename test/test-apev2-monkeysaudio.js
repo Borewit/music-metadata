1222eed82c98a89de52641e37c64a4d2131315a9
Lee Treveil <leetreveil@gmail.com>
2011-05-15 18:38:40 +0100

    Improved apev2 support.

--- test/test-apev2-monkeysaudio.js
+++ test/test-apev2-monkeysaudio.js
@@ -3,7 +3,7 @@ var mm = require('../lib/index'),
     testy = require('testy'),
     assert = testy.assert;
 
-testy.expected = 29;
+testy.expected = 34;
 
 var sample = require('path').join(__dirname, 'samples/monkeysaudio.ape');
 var parser = new mm(fs.createReadStream(sample));
@@ -16,6 +16,8 @@ parser.on('metadata', function(result) {
   assert.strictEqual(result.album, 'Audioslave');
   assert.strictEqual(result.year, '2002');
   assert.strictEqual(result.genre[0], 'Alternative');
+  assert.strictEqual(result.track.no, 7);
+  assert.strictEqual(result.disk.no, 3);
   assert.strictEqual(result.picture[0].format, 'jpg');
   assert.strictEqual(result.picture[0].data.length, 48658);
   assert.strictEqual(result.picture[1].format, 'jpg');
@@ -40,6 +42,14 @@ parser.on('album', function(result) {
   assert.strictEqual(result, 'Audioslave');
 });
 
+parser.on('track', function(result) {
+  assert.strictEqual(result.no, 7);
+});
+
+parser.on('disk', function(result) {
+  assert.strictEqual(result.no, 3);
+});
+
 parser.on('year', function(result) {
   assert.strictEqual(result, '2002');
 });
@@ -53,6 +63,10 @@ parser.on('picture', function(result) {
   assert.strictEqual(result[0].data.length, 48658);
   assert.strictEqual(result[1].format, 'jpg');
   assert.strictEqual(result[1].data.length, 48658);
+});
+
+parser.on('comment', function(result) {
+  assert.strictEqual(result[0], 'This is a sample ape file');
 });
 
 //Raw tests
