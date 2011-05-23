2bd37c8af196c504c7958843741b1b0781467e8c
Lee Treveil <leetreveil@gmail.com>
2011-05-23 22:45:56 +0100

    Improved the deunsyncronization algorithm to be O(n) instead of O(N^2).

--- test/test-deunsync.js
+++ test/test-deunsync.js
@@ -2,15 +2,12 @@ var common = require('../lib/common'),
     testy = require('testy'),
     assert = testy.assert;
     
-testy.expected = 2;
+testy.expected = 1;
     
-//testing that we can deunsync data correctly
-//it shouldn't matter if it's a buffer or not
-//0x00 to the right of 0xFF should be removed
 var expected = new Buffer([0xFF, 0xD8, 0xFF, 0xE0, 0x00]);    
-var sample = [0xFF, 0xD8, 0xFF, 0x00, 0xE0, 0x00];
+var sample = new Buffer([0xFF, 0xD8, 0xFF, 0x00, 0xE0, 0x00]);
+var output = common.removeUnsyncBytes(sample);
 
-assert.deepEqual(expected, common.removeUnsyncBytes(sample));
-assert.deepEqual(expected, new Buffer(common.removeUnsyncBytes(sample)));
+assert.deepEqual(output, expected);
 
 testy.finish();
