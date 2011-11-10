337f2ca3b671613c33ef0a410d4f8f63782ffab1
Lee Treveil <leetreveil@gmail.com>
2011-11-10 16:05:54 +0000

    Fixed test runner when ran from npm test

--- test/alltests.js
+++ test/alltests.js
@@ -1,6 +1,7 @@
 if (module == require.main) {
+
   var spawn = require('child_process').spawn,
-      path = require('path');
+      path  = require('path');
     
   var tests = ['test-genres.js', 'test-deunsync.js', 'test-metadataleak.js',
                'test-id3v1.1.js', 'test-id3v2.2.js', 'test-id3v2.3.js', 
@@ -9,14 +10,14 @@ if (module == require.main) {
                'test-flac.js'];
                
   for (var i=0; i < tests.length; i++) {
-    var fullPath = path.join(__dirname, tests[i]);
-    var test = spawn(process.execPath, [fullPath]);
-
-    test.stdout.on('data', function(data) {
+    var test = tests[i];
+    var testProcess = spawn(process.execPath, [path.join(__dirname, test)]);
+    
+    testProcess.stdout.on('data', function(data) {
       process.stdout.write(data.toString());
     });
     
-    test.stderr.on('data', function(data) {
+    testProcess.stderr.on('data', function(data) {
       process.stderr.write(data.toString());
     }); 
   }
