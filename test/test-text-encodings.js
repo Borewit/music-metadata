c36022668ede4e1178b20a3a398e003349690efc
Lee Treveil <leetreveil@gmail.com>
2013-05-22 20:58:45 +0100

    simpler utf16 string decoder
    flip bytes if big endian and use node core decoder instead of handrolled

---
+++ test/test-text-encodings.js
@@ -0,0 +1,26 @@
+var common = require('../lib/common');
+var test   = require('tap').test;
+
+test('should be able to parse UTF-16BE string with BOM', function (t) {
+  var expected = '2007';
+  var sample = new Buffer([0xFE, 0xFF, 0x00, 0x32, 0x00, 0x30, 0x00, 0x30, 0x00, 0x37]);
+  var output = common.readUTF16String(sample).trim();
+  t.strictEqual(output, expected);
+  t.end();
+});
+
+test('should be able to parse UTF-16LE string with BOM', function (t) {
+  var expected = '2007';
+  var sample = new Buffer([0xFF, 0xFE, 0x32, 0x00, 0x30, 0x00, 0x30, 0x00, 0x37, 0x00]);
+  var output = common.readUTF16String(sample).trim();
+  t.strictEqual(output, expected);
+  t.end();
+});
+
+test('should be able to parse UTF-16LE string without BOM', function (t) {
+  var expected = '2007';
+  var sample = new Buffer([0x32, 0x00, 0x30, 0x00, 0x30, 0x00, 0x37, 0x00]);
+  var output = common.readUTF16String(sample);
+  t.strictEqual(output, expected);
+  t.end();
+});
