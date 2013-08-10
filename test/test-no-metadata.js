e28a4fe91ee9a4b172a56c4673f4c62dc831dcd8
Lee Treveil <leetreveil@gmail.com>
2013-08-11 00:10:35 +0100

    better error message for files without metadata

--- test/test-no-metadata.js
+++ test/test-no-metadata.js
@@ -4,11 +4,12 @@ var id3    = require('../lib/index');
 var test   = require('tap').test;
 
 test('shouldn\'t raise metadata event for files that can\'t be parsed', function (t) {
+  t.plan(1);
   new id3(fs.createReadStream(__filename))
     .on('metadata', function (result) {
       t.notOk(true, 'this should never be hit');
     })
     .on('done', function (err) {
-      t.end();
-    });
+      t.equal(err.message, 'Could not find metadata header');
+    })
 });
