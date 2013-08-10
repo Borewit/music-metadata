c0486fa4fc046732d9ab2caba741d3915e423ba3
Andrew Kelley <superjoe30@gmail.com>
2013-08-10 16:08:30 -0400

    id4: handle end of stream correctly. closes #27

---
+++ test/test-m4a-noeventbug.js
@@ -0,0 +1,17 @@
+var path   = require('path');
+var fs     = require('fs');
+var mm     = require('../lib/index');
+var test   = require('tap').test;
+
+
+test('no events m4a', function(t) {
+  t.plan(2);
+  var sample = path.join(__dirname, 'samples/bug-no-events.m4a');
+  new mm(fs.createReadStream(sample))
+    .on('metadata', function(result) {
+      t.strictEqual(result.title, '');
+    })
+    .on('done', function(err) {
+      t.ok(err == null);
+    })
+});
