2ed9955d7f33e7b72cdb406fe0825dae2cdce9a2
Andrew Kelley <superjoe30@gmail.com>
2013-08-10 17:31:51 -0400

    update to work with master

--- test/test-m4a-noeventbug.js
+++ test/test-m4a-noeventbug.js
@@ -5,11 +5,11 @@ var test   = require('tap').test;
 
 
 test('no events m4a', function(t) {
-  t.plan(2);
+  t.plan(1);
   var sample = path.join(__dirname, 'samples/bug-no-events.m4a');
   new mm(fs.createReadStream(sample))
     .on('metadata', function(result) {
-      t.strictEqual(result.title, '');
+      t.notOk(true, 'this should never be hit');
     })
     .on('done', function(err) {
       t.equal(err.message, "Unexpected end of stream");
