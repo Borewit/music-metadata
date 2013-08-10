3e462fce3c347523aa508f84ed1eaeb9754a9c16
Andrew Kelley <superjoe30@gmail.com>
2013-08-10 16:30:31 -0400

    fix ogg files unexpected end of stream

---
+++ test/test-ogg-noeventbug.js
@@ -0,0 +1,17 @@
+var path   = require('path');
+var fs     = require('fs');
+var mm     = require('../lib/index');
+var test   = require('tap').test;
+
+
+test('no events ogg', function(t) {
+  t.plan(2);
+  var sample = path.join(__dirname, 'samples/bug-no-events.ogg');
+  new mm(fs.createReadStream(sample))
+    .on('metadata', function(result) {
+      t.strictEqual(result.title, '');
+    })
+    .on('done', function(err) {
+      t.equal(err.message, "Unexpected end of stream");
+    })
+});
