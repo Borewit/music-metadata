c9e96c8e0b23d65b7f6335e1c21f43040f08ed98
Andrew Kelley <superjoe30@gmail.com>
2013-08-11 11:43:18 -0400

    fix crash for unrecognized frame

---
+++ test/test-id3v2-unknownframe.js
@@ -0,0 +1,21 @@
+var path   = require('path');
+var fs     = require('fs');
+var Mmd    = require('../');
+var test   = require('tap').test;
+
+test('invalid "Date" frame should not cause crash', function(t) {
+  t.plan(7);
+  var sample = path.join(__dirname, 'samples/bug-id3v2-unknownframe.mp3');
+  new Mmd(fs.createReadStream(sample))
+    .on('metadata', function (result) {
+      t.strictEqual(result.title, 'One', 'title');
+      t.strictEqual(result.artist[0], 'Coheed And Cambria', 'artist');
+      t.strictEqual(result.album, 'Year Of The Black Rainbow', 'album');
+      t.strictEqual(result.year, '2010', 'year');
+      t.strictEqual(result.track.no, 1, 'track no');
+      t.strictEqual(result.genre[0], 'Progressive Rock', 'genre');
+    })
+    .on('done', function(err) {
+      t.ok(err == null);
+    })
+});
