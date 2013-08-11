48573358d1989ae412c4c9eeade714e2ea8df364
Andrew Kelley <superjoe30@gmail.com>
2013-08-11 11:59:07 -0400

    empty year is "" instead of 0. closes #29

--- test/test-id3v2-xheader.js
+++ test/test-id3v2-xheader.js
@@ -4,7 +4,7 @@ var mm     = require('../lib');
 var test   = require('tap').test;
 
 test('should be able to read id3v2 files with extended headers', function (t) {
-  t.plan(1);
+  t.plan(2);
   var sample = path.join(__dirname, 'samples/id3v2-xheader.mp3');
     new mm(fs.createReadStream(sample))
       .on('metadata', function (result) {
@@ -13,12 +13,14 @@ test('should be able to read id3v2 files with extended headers', function (t) {
           artist: [],
           albumartist: [],
           album: '',
-          year: 0,
+          year: "",
           track: { no: 0, of: 0 },
           genre: [],
           disk: { no: 0, of: 0 },
           picture: {}
         }
+        // additional check because deepEqual is not strict
+        t.strictEqual(result.year, expected.year, "year");
         t.deepEqual(result, expected, 'metadata');
     })
     .on('done', function (err) {
