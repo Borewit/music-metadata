f180dbcbfe1d70d1a32500f7d3ce505238700bf5
Lee Treveil <leetreveil@gmail.com>
2014-02-07 02:43:25 +0000

    don't crash without picture

--- example/main.js
+++ example/main.js
@@ -5,11 +5,13 @@ drop(document, function (files) {
   var mm = musicmetadata(files[0])
 
   mm.on('metadata', function (result) {
-    var picture = result.picture[0];
-    var url = URL.createObjectURL(new Blob([picture.data], {'type': 'image/' + picture.format}));
-    var image = document.getElementById('myimg');
-    image.src = url;
-
+    console.log(result);
+    if (result.picture.length > 0) {
+      var picture = result.picture[0];
+      var url = URL.createObjectURL(new Blob([picture.data], {'type': 'image/' + picture.format}));
+      var image = document.getElementById('myimg');
+      image.src = url;
+    }
     var div = document.getElementById('info');
     div.innerText = JSON.stringify(result, undefined, 2);
   })
