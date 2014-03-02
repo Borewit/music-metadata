3fabfb25222709d444c7dd334db3efeb029de146
Lee Treveil <leetreveil@gmail.com>
2014-03-02 17:37:59 +0000

    fix test in firefox

--- test/test-js-regress-GH-14.js
+++ test/test-js-regress-GH-14.js
@@ -2,23 +2,11 @@ var common = require('../lib/common');
 var test   = require('tape');
 
 test('should be able to detect ftypmp42 as a valid mp4 header type', function (t) {
-  var buf = new Buffer(12);
-  buf[0]  = '0x00'
-  buf[1]  = '0x00'
-  buf[2]  = '0x00'
-  buf[3]  = '0x18'
-  buf[4]  = '0x66' // f
-  buf[5]  = '0x74' // t
-  buf[6]  = '0x79' // y
-  buf[7]  = '0x70' // p
-  buf[8]  = '0x6D' // m
-  buf[9]  = '0x70' // p
-  buf[10] = '0x34' // 4
-  buf[11] = '0x32' // 2
+  var buf = new Buffer([0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, 0x6D, 0x70, 0x34, 0x32])
 
   var types = [
     {
-      buf: new Buffer('ftypmp42'),
+      buf: new Buffer([0x66, 0x74, 0x79, 0x70, 0x6D, 0x70, 0x34, 0x32]),
       tag: require('../lib/id4'),
       offset: 4,
     }
