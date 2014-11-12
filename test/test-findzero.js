5996b842bd76a0214ab58abc9f77ad27892ccdd5
Lee Treveil <leetreveil@gmail.com>
2014-11-12 18:37:32 +0000

    Proper handling of utf16 encoded text in APIC/COMM/USLT frames
    Fixes #71

---
+++ test/test-findzero.js
@@ -0,0 +1,36 @@
+var common = require('../lib/common');
+var test = require('prova');
+
+var findZero = common.findZero;
+
+test('find terminator in ascii encoded string', function (t) {
+  var buf = new Buffer([0xFF, 0xFF, 0xFF, 0x00])
+  t.equal(findZero(buf, 0, buf.length, 'ascii'), 3)
+  t.end()
+})
+
+test('find terminator in middle of ascii encoded string', function (t) {
+  var buf = new Buffer([0xFF, 0xFF, 0x00, 0xFF, 0xFF])
+  t.equal(findZero(buf, 0, buf.length, 'ascii'), 2)
+  t.end()
+})
+
+test('return offset to end if nothing is found', function (t) {
+  var buf = new Buffer([0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
+  t.equal(findZero(buf, 0, buf.length, 'ascii'), buf.length)
+  t.end()
+})
+
+
+test('find terminator in utf16le encoded string', function (t) {
+  var buf = new Buffer([0x68, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x6C, 0x00, 0x6F, 0x00, 0x00, 0x00])
+  t.equal(findZero(buf, 0, buf.length, 'utf16'), 10)
+  t.end()
+})
+
+test('find terminator in utf16be encoded string', function (t) {
+  // hell(0x00, 0x00)
+  var buf = new Buffer([0x00, 0x68, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x6C, 0x00, 0x00])
+  t.equal(findZero(buf, 0, buf.length, 'utf16'), 8)
+  t.end()
+})
---
+++ test/test-id3v2-utf16encoded.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var fs = require('fs');
+var id3 = require('..');
+var test = require('prova');
+var bufferEqual = require('buffer-equal');
+
+test('id3v2.4', function (t) {
+  t.plan(10);
+
+  var sample = (process.browser) ?
+    new Blob([fs.readFileSync(__dirname + '/samples/id3v2-utf16.mp3')])
+    : fs.createReadStream(path.join(__dirname, '/samples/id3v2-utf16.mp3'))
+
+  new id3(sample)
+    .on('metadata', function (result) {
+      console.log(result)
+      t.strictEqual(result.title, 'Redial (Feat. LeafRunner and Nowacking)', 'title')
+      t.strictEqual(result.artist[0], 'YourEnigma', 'artist 0')
+      t.strictEqual(result.year, '2014', 'year')
+      t.strictEqual(result.picture[0].format, 'jpg', 'picture 0 format')
+      t.strictEqual(result.picture[0].data.length, 214219, 'picture 0 length')
+      t.ok(
+        bufferEqual(result.picture[0].data.slice(0, 2), new Buffer([0xFF, 0xD8])),
+        'picture 0 JFIF magic header')
+    })
+    .on('COMM', function (result) {
+      t.strictEqual(result.language, 'eng')
+      t.strictEqual(result.short_description, '')
+      t.strictEqual(result.text, 'Visit http://yourenigma.bandcamp.com')
+    })
+    .on('done', function (err) {
+      t.error(err);
+      t.end();
+    });
+})
