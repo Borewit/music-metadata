671e0b0253bac822f1b5f772d4ed51e04b53ea17
Lee Treveil <leetreveil@gmail.com>
2015-10-06 18:07:28 +0100

    Fix concurrent reads cause incorrect image data
    Because buffers in nodejs are mutable and all buffers are really
    just one big slab of memory it's possible to overwrite the data
    in the underlying buffer that a picture's data buffer references.
    
    Fixes #100

---
+++ test/test-concurrent-picture.js
@@ -0,0 +1,17 @@
+var mm = require('..')
+var fs = require('fs')
+var test = require('prova')
+
+test('concurrent-picture', function (t) {
+  t.plan(6)
+  var files = ['test/samples/flac.flac', 'test/samples/flac-bug.flac']
+  files.forEach(function (file) {
+    mm(fs.createReadStream(file), function (err, result) {
+      t.error(err)
+      fs.readFile(file + '.jpg', function (err, data) {
+        t.error(err)
+        t.deepEqual(result.picture[0].data, data)
+      })
+    })
+  })
+})
