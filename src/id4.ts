50e94f03a1ff1f274c0cd3538f30283565b204c5
Borewit <borewit@users.noreply.github.com>
2017-06-28 08:34:16 +0200

    Merged duplicate interface for movie-header-atom (mhdr)

--- src/id4.ts
+++ src/id4.ts
@@ -142,7 +142,7 @@ interface IAtomMvhd extends IAtomMxhd {
  * Interface for the metadata header atom: 'mhdr'
  * Ref: https://developer.apple.com/library/content/documentation/QuickTime/QTFF/Metadata/Metadata.html#//apple_ref/doc/uid/TP40000939-CH1-SW13
  */
-interface IAtom_mhdr {
+interface IMovieHeaderAtom {
 
   /**
    * One byte that is set to 0.
@@ -153,17 +153,10 @@ interface IAtom_mhdr {
    * Three bytes that are set to 0.
    */
   flags: number,
-
   /**
    * A 32-bit unsigned integer indicating the value to use for the item ID of the next item created or assigned an item ID.
    * If the value is all ones, it indicates that future additions will require a search for an unused item ID.
    */
-  nextItemID: number
-}
-
-interface IMovieHeaderAtom {
-  version: number,
-  flags: number,
   nextItemID: number
 }
 
