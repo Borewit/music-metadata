ecb1f86a3eb2af03e4c39d1ec8b1e0170f04f2e4
Borewit <borewit@users.noreply.github.com>
2017-07-13 20:44:21 +0200

    Enabled native output

--- example/typescript.ts
+++ example/typescript.ts
@@ -1,7 +1,7 @@
-import * as mm from '..';
+import * as mm from 'music-metadata';
 import * as util from 'util';
 
-mm.parseFile('../test/samples/MusicBrainz-multiartist [id3v2.4].V2.mp3')
+mm.parseFile('../test/samples/MusicBrainz-multiartist [id3v2.4].V2.mp3', {native: true})
   .then((metadata) => {
     console.log(util.inspect(metadata, {showHidden: false, depth: null}));
   })
