42fcc7859f86833d3220b444e4771f3263494c0f
Borewit <borewit@users.noreply.github.com>
2017-07-17 12:38:56 +0200

    Added ASF DataType enumeration

--- lib/asf/Util.ts
+++ lib/asf/Util.ts
@@ -1,37 +1,12 @@
 import common from '../common';
+import {DataType} from "./AsfObject";
 
 export type AttributeParser = (buf: Buffer) => boolean | string | number | Buffer;
 
 export class Util {
 
-  public static getParserForAttr(i: number): AttributeParser {
+  public static getParserForAttr(i: DataType): AttributeParser {
     return Util.attributeParsers[i];
-  }
-
-  public static parseUnicodeAttr(buf): string {
-    return common.stripNulls(common.decodeString(buf, 'utf16le'));
-  }
-
-  public static parseByteArrayAttr(buf: Buffer): Buffer {
-    const newBuf = new Buffer(buf.length);
-    buf.copy(newBuf);
-    return newBuf;
-  }
-
-  public static parseBoolAttr(buf: Buffer): boolean {
-    return Util.parseWordAttr(buf) === 1;
-  }
-
-  public static parseDWordAttr(buf: Buffer, offset: number = 0): number {
-    return buf.readUInt32LE(offset);
-  }
-
-  public static parseQWordAttr(buf: Buffer, offset: number = 0): number {
-    return Util.readUInt64LE(buf, offset);
-  }
-
-  public static parseWordAttr(buf: Buffer, offset: number = 0): number {
-    return buf.readUInt16LE(offset);
   }
 
   public static readUInt64LE(buffer: Buffer, offset: number): number {
@@ -39,6 +14,10 @@ export class Util {
     const low = buffer.slice(offset + 4, offset + 8).readUInt32LE(0);
     const maxuint32 = Math.pow(2, 32);
     return ((low * maxuint32) + (high >>> 0));
+  }
+
+  public static parseUnicodeAttr(buf): string {
+    return common.stripNulls(common.decodeString(buf, 'utf16le'));
   }
 
   private static attributeParsers: AttributeParser[] = [
@@ -50,4 +29,27 @@ export class Util {
     Util.parseWordAttr,
     Util.parseByteArrayAttr
   ];
+
+  private static parseByteArrayAttr(buf: Buffer): Buffer {
+    const newBuf = new Buffer(buf.length);
+    buf.copy(newBuf);
+    return newBuf;
+  }
+
+  private static parseBoolAttr(buf: Buffer): boolean {
+    return Util.parseWordAttr(buf) === 1;
+  }
+
+  private static parseDWordAttr(buf: Buffer, offset: number = 0): number {
+    return buf.readUInt32LE(offset);
+  }
+
+  private static parseQWordAttr(buf: Buffer, offset: number = 0): number {
+    return Util.readUInt64LE(buf, offset);
+  }
+
+  private static parseWordAttr(buf: Buffer, offset: number = 0): number {
+    return buf.readUInt16LE(offset);
+  }
+
 }
