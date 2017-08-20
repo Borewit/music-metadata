4c3339640d3adac30b0f69088bb1cc5ae75db6e5
Borewit <borewit@users.noreply.github.com>
2017-08-20 21:14:09 +0200

    Files forgotten to check in.

---
+++ src/id3v2/AbstractID3Parser.ts
@@ -0,0 +1,55 @@
+import ReadableStream = NodeJS.ReadableStream;
+import {ITokenParser} from "../ParserFactory";
+import {ITokenizer} from "strtok3";
+import {INativeAudioMetadata, IOptions} from "../index";
+import {ID3v2Token} from "./ID3v2";
+import {ID3v2Parser} from "./ID3v2Parser";
+import {ID3v1Parser} from "../id3v1/ID3v1Parser";
+
+export abstract class AbstractID3v2Parser implements ITokenParser {
+
+  public static startsWithID3v2Header(tokenizer: ITokenizer): Promise<boolean> {
+    return tokenizer.peekToken(ID3v2Token.Header).then((id3Header) => (id3Header.fileIdentifier === 'ID3'));
+  }
+
+  public parse(tokenizer: ITokenizer, options: IOptions): Promise<INativeAudioMetadata> {
+
+    return tokenizer.peekToken(ID3v2Token.Header)
+      .then((id3Header) => (id3Header.fileIdentifier === 'ID3'))
+      .then((isID3) => {
+        if (isID3) {
+          const id3parser = new ID3v2Parser();
+          return id3parser.parse(tokenizer, options);
+        } else {
+          return {}; // Empty native metadata
+        }
+      })
+      .then((id3v2Metadata: INativeAudioMetadata) => {
+        // merge ID3v2 metadata with whatever came after the ID3v2 header
+        return this._parse(tokenizer, options).then((metadata) => {
+          for (const tagType in id3v2Metadata) {
+            metadata.native[tagType] = id3v2Metadata[tagType];
+          }
+          return metadata;
+        });
+      })
+      .then((metadata) => {
+        const id3v1parser = new ID3v1Parser();
+        return id3v1parser.parse(tokenizer).then((id3v1Metadata) => {
+          for (const tagType in id3v1Metadata) {
+            metadata.native[tagType] = id3v1Metadata[tagType];
+          }
+          return metadata;
+        });
+      });
+  }
+
+  /**
+   * Called after ID3v2 headers are parsed
+   * @param tokenizer
+   * @param options
+   * @param prefixSize Number of bytes already parsed since beginning of stream / file
+   * @private
+   */
+  public abstract _parse(tokenizer: ITokenizer, options: IOptions): Promise<INativeAudioMetadata>;
+}
---
+++ src/id3v2/ID3v2.ts
@@ -0,0 +1,132 @@
+import common from '../common';
+import * as Token from "token-types";
+
+/**
+ * The picture type according to the ID3v2 APIC frame
+ * Ref: http://id3.org/id3v2.3.0#Attached_picture
+ */
+export enum AttachedPictureType {
+  'Other',
+  "32x32 pixels 'file icon' (PNG only)",
+  'Other file icon',
+  'Cover (front)',
+  'Cover (back)',
+  'Leaflet page',
+  'Media (e.g. label side of CD)',
+  'Lead artist/lead performer/soloist',
+  'Artist/performer',
+  'Conductor',
+  'Band/Orchestra',
+  'Composer',
+  'Lyricist/text writer',
+  'Recording Location',
+  'During recording',
+  'During performance',
+  'Movie/video screen capture',
+  'A bright coloured fish',
+  'Illustration',
+  'Band/artist logotype',
+  'Publisher/Studio logotype'
+}
+
+export interface IExtendedHeader {
+  // Extended header size
+  size: number;
+  extendedFlags: number;
+  // Size of padding
+  sizeOfPadding: number;
+  // CRC data present
+  crcDataPresent: boolean;
+}
+
+/**
+ * ID3v2 tag header
+ */
+export interface IID3v2header {
+// ID3v2/file identifier   "ID3"
+  fileIdentifier: string,
+  // ID3v2 versionIndex
+  version: {
+    major: number,
+    revision: number
+  },
+  // ID3v2 flags
+  flags: {
+    // Raw flags value
+    raw: number,
+    // Unsynchronisation
+    unsynchronisation: boolean,
+    // Extended header
+    isExtendedHeader: boolean
+    // Experimental indicator
+    expIndicator: boolean,
+    footer: boolean
+  };
+  size: number;
+}
+
+export class ID3v2Token {
+
+  /**
+   * 28 bits (representing up to 256MB) integer, the msb is 0 to avoid 'false syncsignals'.
+   * 4 * %0xxxxxxx
+   */
+  public static UINT32SYNCSAFE = {
+    get: (buf: Buffer, off: number): number => {
+      return buf[off + 3] & 0x7f | ((buf[off + 2]) << 7) |
+        ((buf[off + 1]) << 14) | ((buf[off]) << 21);
+    },
+    len: 4
+  };
+
+  /**
+   * ID3v2 header
+   * Ref: http://id3.org/id3v2.3.0#ID3v2_header
+   * ToDo
+   */
+  public static Header: Token.IGetToken<IID3v2header> = {
+    len: 10,
+
+    get: (buf, off): IID3v2header => {
+      return {
+        // ID3v2/file identifier   "ID3"
+        fileIdentifier: new Token.StringType(3, 'ascii').get(buf, off),
+        // ID3v2 versionIndex
+        version: {
+          major: Token.INT8.get(buf, off + 3),
+          revision: Token.INT8.get(buf, off + 4)
+        },
+        // ID3v2 flags
+        flags: {
+          // Raw flags value
+          raw: Token.INT8.get(buf, off + 4),
+          // Unsynchronisation
+          unsynchronisation: common.strtokBITSET.get(buf, off + 5, 7),
+          // Extended header
+          isExtendedHeader: common.strtokBITSET.get(buf, off + 5, 6),
+          // Experimental indicator
+          expIndicator: common.strtokBITSET.get(buf, off + 5, 5),
+          footer: common.strtokBITSET.get(buf, off + 5, 4)
+        },
+        size: ID3v2Token.UINT32SYNCSAFE.get(buf, off + 6)
+      };
+    }
+  };
+
+  public static ExtendedHeader: Token.IGetToken<IExtendedHeader> = {
+    len: 10,
+
+    get: (buf, off): IExtendedHeader => {
+      return {
+        // Extended header size
+        size: Token.UINT32_BE.get(buf, off),
+        // Extended Flags
+        extendedFlags: Token.UINT16_BE.get(buf, off + 4),
+        // Size of padding
+        sizeOfPadding: Token.UINT32_BE.get(buf, off + 6),
+        // CRC data present
+        crcDataPresent: common.strtokBITSET.get(buf, off + 4, 31)
+      };
+    }
+  };
+}
