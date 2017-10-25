aaa249b71895c803828abeec02c6c0aef266ac7a
Borewit <borewit@users.noreply.github.com>
2017-10-25 20:30:44 +0200

    Code formatting

--- src/id3v1/ID3v1Parser.ts
+++ src/id3v1/ID3v1Parser.ts
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import {INativeTags, IOptions, ITag} from "../index";
 import {ITokenizer} from "strtok3";
@@ -9,34 +9,34 @@ import * as Token from "token-types";
  * Ref: https://de.wikipedia.org/wiki/Liste_der_ID3v1-Genres
  */
 export const Genres = [
-  'Blues', 'Classic Rock', 'Country', 'Dance', 'Disco', 'Funk', 'Grunge', 'Hip-Hop',
-  'Jazz', 'Metal', 'New Age', 'Oldies', 'Other', 'Pop', 'R&B', 'Rap', 'Reggae', 'Rock',
-  'Techno', 'Industrial', 'Alternative', 'Ska', 'Death Metal', 'Pranks', 'Soundtrack',
-  'Euro-Techno', 'Ambient', 'Trip-Hop', 'Vocal', 'Jazz+Funk', 'Fusion', 'Trance',
-  'Classical', 'Instrumental', 'Acid', 'House', 'Game', 'Sound Clip', 'Gospel', 'Noise',
-  'Alt. Rock', 'Bass', 'Soul', 'Punk', 'Space', 'Meditative', 'Instrumental Pop',
-  'Instrumental Rock', 'Ethnic', 'Gothic', 'Darkwave', 'Techno-Industrial',
-  'Electronic', 'Pop-Folk', 'Eurodance', 'Dream', 'Southern Rock', 'Comedy', 'Cult',
-  'Gangsta Rap', 'Top 40', 'Christian Rap', 'Pop/Funk', 'Jungle', 'Native American',
-  'Cabaret', 'New Wave', 'Psychedelic', 'Rave', 'Showtunes', 'Trailer', 'Lo-Fi', 'Tribal',
-  'Acid Punk', 'Acid Jazz', 'Polka', 'Retro', 'Musical', 'Rock & Roll', 'Hard Rock',
-  'Folk', 'Folk/Rock', 'National Folk', 'Swing', 'Fast-Fusion', 'Bebob', 'Latin', 'Revival',
-  'Celtic', 'Bluegrass', 'Avantgarde', 'Gothic Rock', 'Progressive Rock', 'Psychedelic Rock',
-  'Symphonic Rock', 'Slow Rock', 'Big Band', 'Chorus', 'Easy Listening', 'Acoustic', 'Humour',
-  'Speech', 'Chanson', 'Opera', 'Chamber Music', 'Sonata', 'Symphony', 'Booty Bass', 'Primus',
-  'Porn Groove', 'Satire', 'Slow Jam', 'Club', 'Tango', 'Samba', 'Folklore',
-  'Ballad', 'Power Ballad', 'Rhythmic Soul', 'Freestyle', 'Duet', 'Punk Rock', 'Drum Solo',
-  'A Cappella', 'Euro-House', 'Dance Hall', 'Goa', 'Drum & Bass', 'Club-House',
-  'Hardcore', 'Terror', 'Indie', 'BritPop', 'Negerpunk', 'Polsk Punk', 'Beat',
-  'Christian Gangsta Rap', 'Heavy Metal', 'Black Metal', 'Crossover', 'Contemporary Christian',
-  'Christian Rock', 'Merengue', 'Salsa', 'Thrash Metal', 'Anime', 'JPop', 'Synthpop',
-  'Abstract', 'Art Rock', 'Baroque', 'Bhangra', 'Big Beat', 'Breakbeat', 'Chillout',
-  'Downtempo', 'Dub', 'EBM', 'Eclectic', 'Electro', 'Electroclash', 'Emo', 'Experimental',
-  'Garage', 'Global', 'IDM', 'Illbient', 'Industro-Goth', 'Jam Band', 'Krautrock',
-  'Leftfield', 'Lounge', 'Math Rock', 'New Romantic', 'Nu-Breakz', 'Post-Punk', 'Post-Rock',
-  'Psytrance', 'Shoegaze', 'Space Rock', 'Trop Rock', 'World Music', 'Neoclassical', 'Audiobook',
-  'Audio Theatre', 'Neue Deutsche Welle', 'Podcast', 'Indie Rock', 'G-Funk', 'Dubstep',
-  'Garage Rock', 'Psybient'
+  "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop",
+  "Jazz", "Metal", "New Age", "Oldies", "Other", "Pop", "R&B", "Rap", "Reggae", "Rock",
+  "Techno", "Industrial", "Alternative", "Ska", "Death Metal", "Pranks", "Soundtrack",
+  "Euro-Techno", "Ambient", "Trip-Hop", "Vocal", "Jazz+Funk", "Fusion", "Trance",
+  "Classical", "Instrumental", "Acid", "House", "Game", "Sound Clip", "Gospel", "Noise",
+  "Alt. Rock", "Bass", "Soul", "Punk", "Space", "Meditative", "Instrumental Pop",
+  "Instrumental Rock", "Ethnic", "Gothic", "Darkwave", "Techno-Industrial",
+  "Electronic", "Pop-Folk", "Eurodance", "Dream", "Southern Rock", "Comedy", "Cult",
+  "Gangsta Rap", "Top 40", "Christian Rap", "Pop/Funk", "Jungle", "Native American",
+  "Cabaret", "New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal",
+  "Acid Punk", "Acid Jazz", "Polka", "Retro", "Musical", "Rock & Roll", "Hard Rock",
+  "Folk", "Folk/Rock", "National Folk", "Swing", "Fast-Fusion", "Bebob", "Latin", "Revival",
+  "Celtic", "Bluegrass", "Avantgarde", "Gothic Rock", "Progressive Rock", "Psychedelic Rock",
+  "Symphonic Rock", "Slow Rock", "Big Band", "Chorus", "Easy Listening", "Acoustic", "Humour",
+  "Speech", "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony", "Booty Bass", "Primus",
+  "Porn Groove", "Satire", "Slow Jam", "Club", "Tango", "Samba", "Folklore",
+  "Ballad", "Power Ballad", "Rhythmic Soul", "Freestyle", "Duet", "Punk Rock", "Drum Solo",
+  "A Cappella", "Euro-House", "Dance Hall", "Goa", "Drum & Bass", "Club-House",
+  "Hardcore", "Terror", "Indie", "BritPop", "Negerpunk", "Polsk Punk", "Beat",
+  "Christian Gangsta Rap", "Heavy Metal", "Black Metal", "Crossover", "Contemporary Christian",
+  "Christian Rock", "Merengue", "Salsa", "Thrash Metal", "Anime", "JPop", "Synthpop",
+  "Abstract", "Art Rock", "Baroque", "Bhangra", "Big Beat", "Breakbeat", "Chillout",
+  "Downtempo", "Dub", "EBM", "Eclectic", "Electro", "Electroclash", "Emo", "Experimental",
+  "Garage", "Global", "IDM", "Illbient", "Industro-Goth", "Jam Band", "Krautrock",
+  "Leftfield", "Lounge", "Math Rock", "New Romantic", "Nu-Breakz", "Post-Punk", "Post-Rock",
+  "Psytrance", "Shoegaze", "Space Rock", "Trop Rock", "World Music", "Neoclassical", "Audiobook",
+  "Audio Theatre", "Neue Deutsche Welle", "Podcast", "Indie Rock", "G-Funk", "Dubstep",
+  "Garage Rock", "Psybient"
 ];
 
 /**
@@ -67,14 +67,14 @@ const Iid3v1Token: Token.IGetToken<Iid3v1Header> = {
    * @returns ID3v1.1 header if first 3 bytes equals 'TAG', otherwise null is returned
    */
   get: (buf, off): Iid3v1Header => {
-    const header = new Id3v1StringType(3, 'ascii').get(buf, off);
+    const header = new Id3v1StringType(3).get(buf, off);
     return header === "TAG" ? {
       header,
-      title: new Id3v1StringType(30, 'ascii').get(buf, off + 3),
-      artist: new Id3v1StringType(30, 'ascii').get(buf, off + 33),
-      album: new Id3v1StringType(30, 'ascii').get(buf, off + 63),
-      year: new Id3v1StringType(4, 'ascii').get(buf, off + 93),
-      comment: new Id3v1StringType(28, 'ascii').get(buf, off + 97),
+      title: new Id3v1StringType(30).get(buf, off + 3),
+      artist: new Id3v1StringType(30).get(buf, off + 33),
+      album: new Id3v1StringType(30).get(buf, off + 63),
+      year: new Id3v1StringType(4).get(buf, off + 93),
+      comment: new Id3v1StringType(28).get(buf, off + 97),
       // ID3v1.1 separator for track
       zeroByte: Token.UINT8.get(buf, off + 127),
       // track: ID3v1.1 field added by Michael Mutschler
@@ -86,9 +86,13 @@ const Iid3v1Token: Token.IGetToken<Iid3v1Header> = {
 
 class Id3v1StringType extends Token.StringType {
 
+  constructor(len: number) {
+    super(len, "binary");
+  }
+
   public get(buf: Buffer, off: number): string {
     let value = super.get(buf, off);
-    value = value.trim().replace(/\x00/g, '');
+    value = value.trim().replace(/\x00/g, "");
     return value.length > 0 ? value : undefined;
   }
 }
@@ -111,15 +115,15 @@ export class ID3v1Parser {
     return tokenizer.readToken<Iid3v1Header>(Iid3v1Token, tokenizer.fileSize - Iid3v1Token.len).then(header => {
       if (header) {
         const id3: ITag[] = [];
-        for (const id of ['title', 'artist', 'album', 'comment', 'track',  'year']) {
-          if (header[id] && header[id] !== '')
+        for (const id of ["title", "artist", "album", "comment", "track", "year"]) {
+          if (header[id] && header[id] !== "")
             id3.push({id, value: header[id]});
         }
         const genre = ID3v1Parser.getGenre(header.genre);
         if (genre)
-          id3.push({id: 'genre', value: genre});
+          id3.push({id: "genre", value: genre});
         return {
-          'ID3v1.1': id3
+          "ID3v1.1": id3
         };
       } else
         return null;
