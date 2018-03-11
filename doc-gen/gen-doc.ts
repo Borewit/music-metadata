5b060c490195d38f415faac912c25aa3d4650923
Borewit <borewit@users.noreply.github.com>
2018-03-11 14:53:15 +0100

    #65: Add common tag mapping documentation. Probably the table is going to be to wide.

--- doc-gen/gen-doc.ts
+++ doc-gen/gen-doc.ts
@@ -4,19 +4,33 @@ import {assert} from "chai";
 import * as path from 'path';
 import * as fs from "fs";
 
-import {commonTags, isSingleton} from "../lib/common/GenericTagTypes";
+import {commonTags} from "../lib/common/GenericTagTypes";
 import {CombinedTagMapper} from "../src/index";
 
 import * as markDown from "./MarkDown";
 
 type ITagInfoDict = { [key: string]: {description: string}; }
 
+const combinedTagMapper = new CombinedTagMapper();
+
+function getNativeSourceTags(nativeType: string, commonTag: string): string[] {
+
+  const result: string[] = [];
+
+  const tagMapper = combinedTagMapper.tagMappers[nativeType];
+  for (const nativeTag in tagMapper.tagMap) {
+    if (commonTag === tagMapper.tagMap[nativeTag] ) {
+      console.log("%s <= %s:%s", commonTag, nativeType, nativeTag)
+      result.push(nativeTag);
+    }
+  }
+  return result
+}
 
 function run() {
 
   const combinedTagMapper = new CombinedTagMapper();
 
-  // Read Synchrously
   const json = fs.readFileSync(path.join(__dirname, 'common.json'));
   const commonDescriptionDict: ITagInfoDict = JSON.parse(json as any);
 
@@ -24,25 +38,26 @@ function run() {
 
   table.header = new markDown.Row(["Common tag", "n", "Description"]);
 
+  const tagTypes: string[] = [];
+  for (const nativeType in combinedTagMapper.tagMappers) {
+    tagTypes.push(nativeType);
+    table.header.values.push(nativeType);
+  }
+
+
   for (const commonTagKey in commonTags) {
     const tagInfo = commonDescriptionDict[commonTagKey];
     // console.log('common-tag: key=%s, description=%s', commonTagKey, tagInfo.description)
     const multiplicity = commonTags[commonTagKey].multiple ? '*' : '1';
-    table.rows.push(new markDown.Row([commonTagKey, multiplicity, tagInfo.description]));
+
+    const row = new markDown.Row([commonTagKey, multiplicity, tagInfo.description]);
+    for (const nativeType in combinedTagMapper.tagMappers) {
+      row.values.push(getNativeSourceTags(nativeType, commonTagKey).join(", "));
+    }
+    table.rows.push(row);
   }
 
   console.log(table.toString())
-
-  /*
-  // for each tag type
-  for (const nativeType in combinedTagMapper.tagMappers) {
-    const tagMapper = combinedTagMapper.tagMappers[nativeType];
-    for (const nativeTag in tagMapper.tagMap) {
-      const commonType = tagMapper.tagMap[nativeTag];
-      t.isDefined(commonTags[commonType], "Unknown common tagTypes in mapping " + nativeType + "." + nativeTag + " => " + commonType);
-    }
-  }*/
-
 }
 
 run();
