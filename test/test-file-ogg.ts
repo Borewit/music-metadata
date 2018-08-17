20ea67c37d1d23bd5fc94c7487776f1fee99ec5d
Borewit <borewit@users.noreply.github.com>
2018-08-17 11:37:25 +0200

    Restructure unit test files, group file type tests.

--- test/test-file-ogg.ts
+++ test/test-file-ogg.ts
@@ -176,4 +176,26 @@ describe("Parsing Ogg", function() {
     });
   });
 
+  it("check for ogg-multipage-metadata-bug", () => {
+
+    const filename = 'ogg-multipagemetadata-bug.ogg';
+    const filePath = path.join(__dirname, 'samples', filename);
+
+    return mm.parseFile(filePath).then(result => {
+      assert.strictEqual(result.common.title, 'Modestep - To The Stars (Break the Noize & The Autobots Remix)', 'title');
+      assert.strictEqual(result.common.artist, 'Break The Noize & The Autobots', 'artist');
+      assert.strictEqual(result.common.albumartist, 'Modestep', 'albumartist');
+      assert.strictEqual(result.common.album, 'To The Stars', 'album');
+      assert.strictEqual(result.common.date, '2011-01-01', 'year');
+      assert.strictEqual(result.common.track.no, 2, 'track no');
+      assert.strictEqual(result.common.track.of, 5, 'track of');
+      assert.strictEqual(result.common.disk.no, 1, 'disk no');
+      assert.strictEqual(result.common.disk.of, 1, 'disk of');
+      assert.strictEqual(result.common.genre[0], 'Dubstep', 'genre');
+      assert.strictEqual(result.common.picture[0].format, 'image/jpeg', 'picture format');
+      assert.strictEqual(result.common.picture[0].data.length, 207439, 'picture length');
+    });
+
+  });
+
 });
