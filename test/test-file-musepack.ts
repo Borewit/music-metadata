29e1e6008619ad14143d5ec8a20f28d224cc4692
Borewit <borewit@users.noreply.github.com>
2018-11-13 20:56:14 +0100

    #165 Add Musepack (.mpc) SV8 support

--- test/test-file-musepack.ts
+++ test/test-file-musepack.ts
@@ -1,33 +1,67 @@
 import { assert, expect } from 'chai';
-import * as mm from '../src';
 import * as path from 'path';
+import { Parsers } from './metadata-parsers';
 
 describe('Parse Musepack (.mpc)', () => {
 
   const samplePath = path.join(__dirname, 'samples', 'mpc');
 
-  describe('Musepack SV7', () => {
+  describe('Parse Musepack/SV7 with APEv2 header', () => {
 
-    it('Parse Musepack SV7 with APEv2 header', () => {
+    const filePath = path.join(samplePath, 'apev2.sv7.mpc');
 
-      const filePath = path.join(samplePath, 'apev2.sv7.mpc');
+    Parsers.forEach(parser => {
+      it(parser.description, () => {
 
-      return mm.parseFile(filePath, {native: true}).then(metadata => {
+        return parser.initParser(filePath, 'audio/musepack', {native: true}).then(metadata => {
+          // Check format
+          assert.deepEqual(metadata.format.dataformat, 'Musepack, SV7');
+          assert.strictEqual(metadata.format.sampleRate, 44100);
+          assert.strictEqual(metadata.format.numberOfSamples, 11940);
+          assert.approximately(metadata.format.bitrate, 269649, 1);
+          assert.strictEqual(metadata.format.encoder, '1.15');
 
-        // Check format
-        assert.deepEqual(metadata.format.dataformat, 'Musepack/SV7');
-        assert.strictEqual(metadata.format.sampleRate, 44100);
-        assert.strictEqual(metadata.format.numberOfSamples, 11940);
-        assert.approximately(metadata.format.bitrate, 269649, 1);
-        assert.strictEqual(metadata.format.encoder, '1.15');
-
-        // Check format
-        assert.strictEqual(metadata.common.title, 'God Inside');
-        assert.strictEqual(metadata.common.artist, 'Faze Action');
-        assert.strictEqual(metadata.common.album, 'Broad Souls');
-        assert.strictEqual(metadata.common.date, '2004');
-        assert.deepEqual(metadata.common.track, {no: 9, of: null});
+          // Check generic metadata
+          assert.strictEqual(metadata.common.title, 'God Inside');
+          assert.strictEqual(metadata.common.artist, 'Faze Action');
+          assert.strictEqual(metadata.common.album, 'Broad Souls');
+          assert.strictEqual(metadata.common.date, '2004');
+          assert.deepEqual(metadata.common.track, {no: 9, of: null});
+        });
       });
     });
+
   });
+
+  describe('Parse Musepack/SV8 with APEv2 header', () => {
+
+    const filePath = path.join(samplePath, 'bach-goldberg-variatians-05.sv8.mpc');
+
+    Parsers.forEach(parser => {
+      it(parser.description, () => {
+
+        return parser.initParser(filePath, 'audio/musepack', {native: true}).then(metadata => {
+          // Check format
+          assert.deepEqual(metadata.format.dataformat, 'Musepack, SV8');
+          assert.strictEqual(metadata.format.sampleRate, 48000);
+          assert.strictEqual(metadata.format.numberOfSamples, 24000);
+          assert.strictEqual(metadata.format.numberOfChannels, 2);
+          assert.approximately(metadata.format.duration, 0.5, 1 / 2000);
+          assert.approximately(metadata.format.bitrate, 32368, 1);
+
+          // Check generic metadata
+          assert.strictEqual(metadata.common.title, 'Goldberg Variations, BWV 988: Variatio 4 a 1 Clav.');
+          assert.strictEqual(metadata.common.artist, 'Johann Sebastian Bach');
+          assert.deepEqual(metadata.common.artists, ['Johann Sebastian Bach']);
+          assert.deepEqual(metadata.common.isrc, ['QMNYZ1200005']);
+          assert.deepEqual(metadata.common.license, 'https://creativecommons.org/publicdomain/zero/1.0/');
+          assert.strictEqual(metadata.common.album, 'Open Goldberg Variations');
+          assert.strictEqual(metadata.common.date, '2012-05-28');
+          assert.deepEqual(metadata.common.track, {no: 5, of: 32});
+        });
+      });
+    });
+
+  });
+
 });
