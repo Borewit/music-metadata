104b9057a17981efb786fa36d3e2097489cd62c0
Borewit <borewit@users.noreply.github.com>
2019-02-03 09:54:57 +0100

    Fix lint errors in examples

--- example/typescript/typescript.ts
+++ example/typescript/typescript.ts
@@ -2,9 +2,9 @@ import * as mm from '../../src';
 import * as util from 'util';
 
 mm.parseFile('../test/samples/MusicBrainz-multiartist [id3v2.4].V2.mp3', {native: true})
-  .then((metadata) => {
+  .then(metadata => {
     console.log(util.inspect(metadata, {showHidden: false, depth: null}));
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(err.message);
   });
