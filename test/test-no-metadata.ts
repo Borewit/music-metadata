68595fe7dc4ba095da9deb627437b0b351400794
Borewit <borewit@users.noreply.github.com>
2019-08-21 14:34:02 +0200

    Remove unused variable, rewrote test case with async/await.

--- test/test-no-metadata.ts
+++ test/test-no-metadata.ts
@@ -4,12 +4,16 @@ import * as path from 'path';
 
 const t = assert;
 
-it("should reject files that can't be parsed", () => {
+it("should reject files that can't be parsed", async () => {
 
   const filePath = path.join(__dirname, 'samples', __filename);
 
   // Run with default options
-  return mm.parseFile(filePath).then(result => {
-    throw new Error("Should reject a file which cannot be parsed");
-  }).catch(err => null);
+  try {
+    await mm.parseFile(filePath);
+    assert.fail('Should reject a file which cannot be parsed');
+  } catch(err) {
+    assert.isDefined(err);
+    assert.isDefined(err.message);
+  }
 });
