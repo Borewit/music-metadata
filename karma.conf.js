3bb10a28d2112b97a90efe080cc93e1c2999820a
Borewit <borewit@users.noreply.github.com>
2019-10-19 11:39:26 +0200

    Code style fixes

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const path = require('path')
+const path = require('path');
 
 module.exports = config => {
   config.set({
