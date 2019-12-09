d54b3d50e0e26bfdbedbd2576ae8eb7d7eae16e9
Borewit <borewit@users.noreply.github.com>
2019-12-09 20:05:37 +0100

    Improve tsdoc

--- lib/index.ts
+++ lib/index.ts
@@ -15,10 +15,10 @@ export { parseFromTokenizer } from './core';
 
 /**
  * Parse audio from Node Stream.Readable
- * @param {Stream.Readable} stream Stream to read the audio track from
- * @param {string} mimeType Content specification MIME-type, e.g.: 'audio/mpeg'
- * @param {IOptions} options Parsing options
- * @returns {Promise<IAudioMetadata>}
+ * @param stream - Stream to read the audio track from
+ * @param mimeType - Content specification MIME-type, e.g.: 'audio/mpeg'
+ * @param options - Parsing options
+ * @returns Metadata
  */
 export async function parseStream(stream: Stream.Readable, mimeType?: string, options: IOptions = {}): Promise<IAudioMetadata> {
   const tokenizer = await strtok3.fromStream(stream);
@@ -27,19 +27,19 @@ export async function parseStream(stream: Stream.Readable, mimeType?: string, op
 
 /**
  * Parse audio from Node Buffer
- * @param {Stream.Readable} stream Audio input stream
- * @param {string} mimeType <string> Content specification MIME-type, e.g.: 'audio/mpeg'
- * @param {IOptions} options Parsing options
- * @returns {Promise<IAudioMetadata>}
+ * @param stream - Audio input stream
+ * @param mimeType - Content specification MIME-type, e.g.: 'audio/mpeg'
+ * @param options - Parsing options
+ * @returns Metadata
  * Ref: https://github.com/Borewit/strtok3/blob/e6938c81ff685074d5eb3064a11c0b03ca934c1d/src/index.ts#L15
  */
 export const parseBuffer = Core.parseBuffer;
 
 /**
  * Parse audio from Node file
- * @param {string} filePath Media file to read meta-data from
- * @param {IOptions} options Parsing options
- * @returns {Promise<IAudioMetadata>}
+ * @param filePath - Media file to read meta-data from
+ * @param options - Parsing options
+ * @returns Metadata
  */
 export async function parseFile(filePath: string, options: IOptions = {}): Promise<IAudioMetadata> {
 
@@ -67,14 +67,14 @@ export async function parseFile(filePath: string, options: IOptions = {}): Promi
 
 /**
  * Create a dictionary ordered by their tag id (key)
- * @param {ITag[]} nativeTags list of tags
- * @returns {INativeTagDict} Tags indexed by id
+ * @param nativeTags - List of tags
+ * @returns Tags indexed by id
  */
 export const orderTags = Core.orderTags;
 
 /**
  * Convert rating to 1-5 star rating
- * @param {number} rating Normalized rating [0..1] (common.rating[n].rating)
- * @returns {number} Number of stars: 1, 2, 3, 4 or 5 stars
+ * @param rating - Normalized rating [0..1] (common.rating[n].rating)
+ * @returns Number of stars: 1, 2, 3, 4 or 5 stars
  */
 export const ratingToStars = Core.ratingToStars;
