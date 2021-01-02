39e8f77756ec9261e61653da3e8b5ecd7aec85b0
Borewit <borewit@users.noreply.github.com>
2021-01-02 19:27:17 +0100

    Fix examples and update to ECMA version 8.

--- example/javascript/stream.js
+++ example/javascript/stream.js
@@ -5,8 +5,8 @@ const mm = require('../../lib'); // music-metadata
 const audioUrl = 'https://github.com/Borewit/music-metadata/raw/master/test/samples/MusicBrainz%20-%20Beth%20Hart%20-%20Sinner\'s%20Prayer%20%5Bid3v2.3%5D.V2.mp3';
 
 function httpGet (url) {
-  return new Promise(function (resolve, reject) {
-    https.get(url, function (res) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
       switch (res.statusCode) {
         case 200:
           resolve(res);
@@ -21,17 +21,13 @@ function httpGet (url) {
   });
 }
 
-// Stream MP3 sample file from GitHub via HTTP
-httpGet(audioUrl).then(metadata => {
-  // Parse the MP3 audio stream
-  const mimeType = metadata.headers['content-type'];
-  console.log('Parsing: ' + mimeType);
-  return mm.parseStream(metadata, mimeType, {native: true})
-    .then(metadata => {
-      // Print the metadata result
-      console.log(util.inspect(metadata, {showHidden: false, depth: null}));
-    });
-}).catch(function (err) {
-  // Oops, something went wrong
-  console.error(err.message);
-});
+(async () => {
+  try {
+    // Stream MP3 sample file from GitHub via HTTP
+    const metadata = await httpGet(audioUrl);
+    console.log(util.inspect(metadata, { showHidden: false, depth: null }));
+  } catch(err) {
+    // Oops, something went wrong
+    console.error(err.message);
+  }
+})();
--- example/javascript/walk.js
+++ example/javascript/walk.js
@@ -1,33 +1,36 @@
-const walk = require('walk');
+const Walk = require('@root/walk');
 const path = require('path');
+const util = require('util');
 const mm = require('../../lib'); // music-metadata
 
-const walker = walk.walk('M:\\');
+async function walkFunc(err, pathname, dirent) {
+  // err is failure to lstat a file or directory
+  // pathname is relative path, including the file or folder name
+  // dirent = { name, isDirectory(), isFile(), isSymbolicLink(), ... }
 
-let filesParsed = 0;
-
-walker.on('file', function (root, fileStats, next) {
-
-  switch (path.extname(fileStats.name)) {
-    case '.mp3':
-    case '.m4a':
-    case '.wav':
-    case '.ogg':
-    case '.flac':
-      // Queue (asynchronous call) parsing of metadata
-      const fn = path.join(root, fileStats.name);
-      mm.parseFile(fn).then(metadata => {
-        // console.log(util.inspect(metadata, { showHidden: false, depth: null }));
-        console.log('Parsed %s files, last file: %s', ++filesParsed, fn);
-        next(); // asynchronous call completed, 'release' next
-      }).catch(function (err) {
-        console.error('Error parsing:  %s', fn);
-        console.error(err.message);
-        next();
-      });
-      break;
-
-    default:
-      next(); // Go to next file, no asynchronous call 'queued'
+  if (dirent.isFile()) {
+    console.log(`File: ${pathname}`);
+    switch (path.extname(pathname)) {
+      case '.mp3':
+      case '.m4a':
+      case '.wav':
+      case '.ogg':
+      case '.flac':
+        // Queue (asynchronous call) parsing of metadata
+        const metadata = await mm.parseFile(pathname);
+        console.log(util.inspect(metadata, { showHidden: false, depth: null }));
+        break;
+    }
   }
-});
+}
+
+(async () => {
+  try {
+    await Walk.walk('M:\\_Classic', walkFunc);
+    console.log('Done.');
+  } catch(err) {
+    // Oops, something went wrong
+    console.error(err.message);
+  }
+})();
+
