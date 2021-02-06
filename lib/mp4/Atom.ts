2a090907c348b954cb911748a69591b5e0b841bb
Borewit <borewit@users.noreply.github.com>
2021-02-06 19:49:06 +0100

    Borewit/music-metadata#749: Handle box (atom) with 0 length.

--- lib/mp4/Atom.ts
+++ lib/mp4/Atom.ts
@@ -3,13 +3,13 @@ import * as initDebug from 'debug';
 
 import * as AtomToken from './AtomToken';
 
-export type AtomDataHandler = (atom: Atom) => Promise<void>;
+export type AtomDataHandler = (atom: Atom, remaining: number) => Promise<void>;
 
 const debug = initDebug('music-metadata:parser:MP4:Atom');
 
 export class Atom {
 
-  public static async readAtom(tokenizer: ITokenizer, dataHandler: AtomDataHandler, parent: Atom): Promise<Atom> {
+  public static async readAtom(tokenizer: ITokenizer, dataHandler: AtomDataHandler, parent: Atom, remaining: number): Promise<Atom> {
 
     // Parse atom header
     const offset = tokenizer.position;
@@ -19,13 +19,10 @@ export class Atom {
     if (extended) {
       header.length = await tokenizer.readToken<number>(AtomToken.ExtendedSize);
     }
-    const extendToEOF = ((header.length === 0) && (header.name === 'mdat') && !parent);
-    if (extendToEOF) {
-        header.length = tokenizer.fileInfo.size - tokenizer.position + 8;
-    }
-    const atomBean = new Atom(header, extended, parent);
+    const atomBean = new Atom(header, header.length === 1, parent);
+    const payloadLength = atomBean.getPayloadLength(remaining);
     debug(`parse atom name=${atomBean.atomPath}, extended=${atomBean.extended}, offset=${offset}, len=${atomBean.header.length}`); //  buf.toString('ascii')
-    await atomBean.readData(tokenizer, dataHandler);
+    await atomBean.readData(tokenizer, dataHandler, payloadLength);
     return atomBean;
   }
 
@@ -41,19 +38,19 @@ export class Atom {
     return this.extended ? 16 : 8;
   }
 
-  public getPayloadLength(): number {
-    return this.header.length - this.getHeaderLength();
+  public getPayloadLength(remaining: number): number {
+    return (this.header.length === 0 ? remaining : this.header.length) - this.getHeaderLength();
   }
 
   public async readAtoms(tokenizer: ITokenizer, dataHandler: AtomDataHandler, size: number): Promise<void> {
     while (size > 0) {
-      const atomBean = await Atom.readAtom(tokenizer, dataHandler, this);
+      const atomBean = await Atom.readAtom(tokenizer, dataHandler, this, size);
       this.children.push(atomBean);
-      size -= atomBean.header.length;
+      size -= atomBean.header.length === 0 ? size : atomBean.header.length;
     }
   }
 
-  private async readData(tokenizer: ITokenizer, dataHandler: AtomDataHandler): Promise<void> {
+  private async readData(tokenizer: ITokenizer, dataHandler: AtomDataHandler, remaining: number): Promise<void> {
     switch (this.header.name) {
       // "Container" atoms, contains nested atoms
       case 'moov': // The Movie Atom: contains other atoms
@@ -65,12 +62,12 @@ export class Atom {
       case '<id>':
       case 'ilst':
       case 'tref':
-        return this.readAtoms(tokenizer, dataHandler, this.getPayloadLength());
+        return this.readAtoms(tokenizer, dataHandler, this.getPayloadLength(remaining));
 
       case 'meta': // Metadata Atom, ref: https://developer.apple.com/library/content/documentation/QuickTime/QTFF/Metadata/Metadata.html#//apple_ref/doc/uid/TP40000939-CH1-SW8
         // meta has 4 bytes of padding, ignore
         await tokenizer.ignore(4);
-        return this.readAtoms(tokenizer, dataHandler, this.getPayloadLength() - 4);
+        return this.readAtoms(tokenizer, dataHandler, this.getPayloadLength(remaining) - 4);
 
       case 'mdhd': // Media header atom
       case 'mvhd': // 'movie' => 'mvhd': movie header atom; child of Movie Atom
@@ -78,7 +75,7 @@ export class Atom {
       case 'stsz':
       case 'mdat':
       default:
-        return dataHandler(this);
+        return dataHandler(this, remaining);
     }
   }
 }
