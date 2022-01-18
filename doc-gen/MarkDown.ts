689fe486a0deb4537b512dfcccedca2ed524bf4b
Borewit <borewit@users.noreply.github.com>
2022-01-18 18:51:52 +0100

    Normalize single quote import style

--- doc-gen/MarkDown.ts
+++ doc-gen/MarkDown.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+import * as fs from 'fs';
 
 export class Row {
   constructor(public values: string[]) {
@@ -7,7 +7,7 @@ export class Row {
 
 export class Table {
 
-  private static padEnd(value: string, size: number, pad: string = " ") {
+  private static padEnd(value: string, size: number, pad: string = ' ') {
     while (value.length < size) {
       value += pad;
     }
@@ -17,16 +17,16 @@ export class Table {
   private static writeRow(out: fs.WriteStream, values: string[], colSizes: number[]) {
     const colValues: string[] = [];
     for (let ci = 0; ci < colSizes.length; ++ci) {
-      const cellTxt = values.length > ci ? values[ci] : "";
+      const cellTxt = values.length > ci ? values[ci] : '';
       colValues.push(Table.padEnd(cellTxt, colSizes[ci]));
     }
-    out.write("| " + colValues.join(" | ") + " |\n");
+    out.write('| ' + colValues.join(' | ') + ' |\n');
   }
 
   private static lineToString(colSizes: number[]): string {
 
-    const colValues = colSizes.map(size => Table.padEnd("-", size, "-"));
-    return "|-" + colValues.join("-|-") + "-|\n";
+    const colValues = colSizes.map(size => Table.padEnd('-', size, '-'));
+    return '|-' + colValues.join('-|-') + '-|\n';
   }
 
   public rows: Row[] = [];
