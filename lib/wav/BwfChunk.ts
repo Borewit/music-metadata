Borewit/music-metadata

commit 2a26d925f206de7884bb6b69068f411497acb199

Add initial support for Broadcast Wave Format (BWF)

lib/wav/BwfChunk.ts
@@ -1,0 +1,40 @@
+import { IGetToken } from 'strtok3/lib/core';
+import * as Token from 'token-types';
+
+export interface IBroadcastAudioExtensionChunk {
+  description: string;
+  originator: string;
+  originatorReference: string;
+  originationDate: string;
+  originationTime: string;
+  timeReferenceLow: number,
+  timeReferenceHigh: number,
+  version: number,
+  umid: Buffer,
+}
+
+/**
+ * Broadcast Audio Extension Chunk
+ * Ref: https://tech.ebu.ch/docs/tech/tech3285.pdf
+ */
+export const BroadcastAudioExtensionChunk: IGetToken<IBroadcastAudioExtensionChunk> = {
+  len: 420,
+
+  get: (buf, off) => {
+    return {
+      description: new Token.StringType(256, 'ascii').get(buf, off).trim(),
+      originator: new Token.StringType(32, 'ascii').get(buf, off + 256).trim(),
+      originatorReference: new Token.StringType(32, 'ascii').get(buf, off + 288).trim(),
+      originationDate: new Token.StringType(10, 'ascii').get(buf, off + 320).trim(),
+      originationTime: new Token.StringType(8, 'ascii').get(buf, off + 330).trim(),
+      timeReferenceLow: Token.UINT32_LE.get(buf, off + 338),
+      timeReferenceHigh: Token.UINT32_LE.get(buf, off + 342),
+      version: Token.UINT16_LE.get(buf, off + 346),
+      umid: new Token.BufferType(64).get(buf, off + 348),
+      loudnessValue: Token.UINT16_LE.get(buf, off + 412),
+      maxTruePeakLevel: Token.UINT16_LE.get(buf, off + 414),
+      maxMomentaryLoudness: Token.UINT16_LE.get(buf, off + 416),
+      maxShortTermLoudness: Token.UINT16_LE.get(buf, off + 418)
+    };
+  }
+};

lib/ParserFactory.ts
@@ -154,6 +154,7 @@ export class ParserFactory {
         return 'aiff';
 
       case '.wav':
+      case '.bwf': // Broadcast Wave Format
         return 'riff';
 
       case '.wv':

lib/wav/WaveParser.ts
@@ -9,6 +9,7 @@ import { ID3v2Parser } from '../id3v2/ID3v2Parser';
 import Common from '../common/Util';
 import { FourCcToken } from '../common/FourCC';
 import { BasicParser } from '../common/BasicParser';
+import { BroadcastAudioExtensionChunk } from '../wav/BwfChunk';
 
 const debug = initDebug('music-metadata:parser:RIFF');
 
@@ -107,6 +108,19 @@ export class WaveParser extends BasicParser {
           }
 
           this.metadata.setFormat('bitrate', this.metadata.format.numberOfChannels * this.blockAlign * this.metadata.format.sampleRate); // ToDo: check me
+          await this.tokenizer.ignore(header.chunkSize);
+          break;
+
+        case 'bext': // Broadcast Audio Extension chunk	https://tech.ebu.ch/docs/tech/tech3285.pdf
+          const bext = await this.tokenizer.readToken(BroadcastAudioExtensionChunk);
+          Object.keys(bext).forEach(key => {
+            this.metadata.addTag('exif', 'bext.' + key, bext[key]);
+          });
+          break;
+
+        case '\x00\x00\x00\x00': // padding ??
+          debug(`Ignore padding chunk: RIFF/${header.chunkID} of ${header.chunkSize} bytes`);
+          this.metadata.addWarning('Ignore chunk: RIFF/' + header.chunkID);
           await this.tokenizer.ignore(header.chunkSize);
           break;
 

commit 82f864043d17e0254f5482a36491c04bf752f504

Fix bug commit 2a26d925f206de7884bb6b69068f411497acb199 introduced

lib/wav/BwfChunk.ts
@@ -10,7 +10,7 @@ export interface IBroadcastAudioExtensionChunk {
   timeReferenceLow: number,
   timeReferenceHigh: number,
   version: number,
-  umid: Buffer,
+  umid: Uint8Array,
 }
 
 /**
@@ -20,21 +20,21 @@ export interface IBroadcastAudioExtensionChunk {
 export const BroadcastAudioExtensionChunk: IGetToken<IBroadcastAudioExtensionChunk> = {
   len: 420,
 
-  get: (buf, off) => {
+  get: (uint8array, off) => {
     return {
-      description: new Token.StringType(256, 'ascii').get(buf, off).trim(),
-      originator: new Token.StringType(32, 'ascii').get(buf, off + 256).trim(),
-      originatorReference: new Token.StringType(32, 'ascii').get(buf, off + 288).trim(),
-      originationDate: new Token.StringType(10, 'ascii').get(buf, off + 320).trim(),
-      originationTime: new Token.StringType(8, 'ascii').get(buf, off + 330).trim(),
-      timeReferenceLow: Token.UINT32_LE.get(buf, off + 338),
-      timeReferenceHigh: Token.UINT32_LE.get(buf, off + 342),
-      version: Token.UINT16_LE.get(buf, off + 346),
-      umid: new Token.BufferType(64).get(buf, off + 348),
-      loudnessValue: Token.UINT16_LE.get(buf, off + 412),
-      maxTruePeakLevel: Token.UINT16_LE.get(buf, off + 414),
-      maxMomentaryLoudness: Token.UINT16_LE.get(buf, off + 416),
-      maxShortTermLoudness: Token.UINT16_LE.get(buf, off + 418)
+      description: new Token.StringType(256, 'ascii').get(uint8array, off).trim(),
+      originator: new Token.StringType(32, 'ascii').get(uint8array, off + 256).trim(),
+      originatorReference: new Token.StringType(32, 'ascii').get(uint8array, off + 288).trim(),
+      originationDate: new Token.StringType(10, 'ascii').get(uint8array, off + 320).trim(),
+      originationTime: new Token.StringType(8, 'ascii').get(uint8array, off + 330).trim(),
+      timeReferenceLow: Token.UINT32_LE.get(uint8array, off + 338),
+      timeReferenceHigh: Token.UINT32_LE.get(uint8array, off + 342),
+      version: Token.UINT16_LE.get(uint8array, off + 346),
+      umid: new Token.Uint8ArrayType(64).get(uint8array, off + 348),
+      loudnessValue: Token.UINT16_LE.get(uint8array, off + 412),
+      maxTruePeakLevel: Token.UINT16_LE.get(uint8array, off + 414),
+      maxMomentaryLoudness: Token.UINT16_LE.get(uint8array, off + 416),
+      maxShortTermLoudness: Token.UINT16_LE.get(uint8array, off + 418)
     };
   }
 };