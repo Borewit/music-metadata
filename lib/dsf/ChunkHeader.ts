85196db49c8ff7e0535c7843cfb71f09a1dbb319
mikiymk <mikiymk001@gmail.com>
2022-06-13 16:33:47 +0900

    dsfを分割

---
+++ lib/dsf/ChunkHeader.ts
@@ -0,0 +1,33 @@
+import * as Token from "../token-types";
+import { IGetToken } from "../strtok3";
+
+import { FourCcToken } from "../common/FourCC";
+
+/**
+ * Common interface for the common chunk DSD header
+ */
+export interface IChunkHeader {
+  /**
+   * Chunk ID
+   */
+  id: string;
+
+  /**
+   * Chunk size
+   */
+  size: bigint;
+}
+
+/**
+ * Common chunk DSD header: the 'chunk name (Four-CC)' & chunk size
+ */
+export const ChunkHeader: IGetToken<IChunkHeader> = {
+  len: 12,
+
+  get: (buf: Uint8Array, off: number): IChunkHeader => {
+    return {
+      id: FourCcToken.get(buf, off),
+      size: Token.UINT64_LE.get(buf, off + 4),
+    };
+  },
+};
