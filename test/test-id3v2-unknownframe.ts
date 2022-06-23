db7eb5dc290e7c9cc77d793da60e4b2e950fb4a6
mikiymk <mikiymk001@gmail.com>
2022-06-23 17:53:17 +0900

    test-id3v2-unknownframeを更新

--- test/test-id3v2-unknownframe.ts
+++ test/test-id3v2-unknownframe.ts
@@ -1,25 +1,20 @@
-import { assert, it } from "vitest";
-import * as path from "node:path";
+import { expect, test } from "vitest";
+import { join } from "node:path";
 
-import * as mm from "../lib";
+import { parseFile } from "../lib";
 import { samplePath } from "./util";
 
-const t = assert;
-
-it('invalid "Date" frame should not cause crash', () => {
+test('invalid "Date" frame should not cause crash', async () => {
   const filename = "bug-id3v2-unknownframe.mp3";
-  const filePath = path.join(samplePath, filename);
+  const filePath = join(samplePath, filename);
 
-  function checkCommon(common: mm.ICommonTagsResult) {
-    t.strictEqual(common.title, "One", "common.title");
-    t.strictEqual(common.artist, "Coheed And Cambria", "common.artist");
-    t.strictEqual(common.album, "Year Of The Black Rainbow", "common.album");
-    t.strictEqual(common.year, 2010, "common.year");
-    t.deepEqual(common.track, { no: 1, of: null }, "common.track");
-    t.deepEqual(common.genre, ["Progressive Rock"], "common.genre");
-  }
+  const metadata = await parseFile(filePath, { duration: true });
+  const common = metadata.common;
 
-  return mm.parseFile(filePath, { duration: true }).then((metadata) => {
-    checkCommon(metadata.common);
-  });
+  expect(common.title, "common.title").toBe("One");
+  expect(common.artist, "common.artist").toBe("Coheed And Cambria");
+  expect(common.album, "common.album").toBe("Year Of The Black Rainbow");
+  expect(common.year, "common.year").toBe(2010);
+  expect(common.track, "common.track").toStrictEqual({ no: 1, of: null });
+  expect(common.genre, "common.genre").toStrictEqual(["Progressive Rock"]);
 });
