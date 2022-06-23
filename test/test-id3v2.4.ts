4b382e0ca4ee013f565cb149c7361eb37a099008
mikiymk <mikiymk001@gmail.com>
2022-06-23 19:22:32 +0900

    test-id3v2.4を更新

--- test/test-id3v2.4.ts
+++ test/test-id3v2.4.ts
@@ -1,137 +1,92 @@
-import { describe, assert, it } from "vitest";
-import * as mm from "../lib";
+import { test, expect } from "vitest";
+import { parseFile } from "../lib";
 
-import * as path from "node:path";
+import { join } from "node:path";
 import { samplePath } from "./util";
 
-const t = assert;
+test("should decode id3v2.4", async () => {
+  const filename = "id3v2.4.mp3";
+  const filePath = join(samplePath, filename);
 
-describe("Decode MP3/ID3v2.4", () => {
-  it("should decode id3v2.4", () => {
-    const filename = "id3v2.4.mp3";
-    const filePath = path.join(samplePath, filename);
+  const metadata = await parseFile(filePath, { duration: true });
+  const format = metadata.format;
+  const common = metadata.common;
 
-    return mm.parseFile(filePath, { duration: true }).then((metadata) => {
-      t.deepEqual(
-        metadata.format.tagTypes,
-        ["ID3v2.4", "ID3v1"],
-        "format.tagTypes"
-      );
-      t.strictEqual(
-        metadata.format.duration,
-        0.783_673_469_387_755_1,
-        "format.format.duration"
-      );
-      t.strictEqual(
-        metadata.format.sampleRate,
-        44_100,
-        "format.sampleRate = 44.1 kHz"
-      );
-      t.strictEqual(
-        metadata.format.bitrate,
-        128_000,
-        "format.bitrate = 128 kbit/sec"
-      );
-      t.strictEqual(
-        metadata.format.codecProfile,
-        "CBR",
-        "format.codecProfile = CBR"
-      );
-      t.strictEqual(metadata.format.container, "MPEG", "format.container");
-      t.strictEqual(metadata.format.codec, "MPEG 1 Layer 3", "format.codec");
-      t.strictEqual(metadata.format.tool, "LAME 3.98r", "format.tool");
-      t.strictEqual(
-        metadata.format.numberOfChannels,
-        2,
-        "format.numberOfChannels = 2"
-      );
+  expect(format.tagTypes, "format.tagTypes").toStrictEqual([
+    "ID3v2.4",
+    "ID3v1",
+  ]);
+  expect(format.duration, "format.format.duration").toBe(
+    0.783_673_469_387_755_1
+  );
+  expect(format.sampleRate, "format.sampleRate = 44.1 kHz").toBe(44_100);
+  expect(format.bitrate, "format.bitrate = 128 kbit/sec").toBe(128_000);
+  expect(format.codecProfile, "format.codecProfile = CBR").toBe("CBR");
+  expect(format.container, "format.container").toBe("MPEG");
+  expect(format.codec, "format.codec").toBe("MPEG 1 Layer 3");
+  expect(format.tool, "format.tool").toBe("LAME 3.98r");
+  expect(format.numberOfChannels, "format.numberOfChannels = 2").toBe(2);
 
-      t.strictEqual(metadata.common.title, "Home", "title");
-      t.strictEqual(metadata.common.artist, "Explo", "common.artist");
-      t.deepEqual(
-        metadata.common.artists,
-        ["Explo", "ions", "nodejsftws"],
-        "common.artists"
-      );
-      t.strictEqual(metadata.common.albumartist, "Soundtrack", "albumartist");
-      t.strictEqual(
-        metadata.common.album,
-        "Friday Night Lights [Original Movie Soundtrack]",
-        "album"
-      );
-      t.strictEqual(metadata.common.year, 2004, "year");
-      t.deepEqual(metadata.common.track, { no: 5, of: null }, "common.track");
-      t.deepEqual(metadata.common.disk, { no: 1, of: 1 }, "common.disk");
-      t.deepEqual(
-        metadata.common.genre,
-        ["Soundtrack", "OST"],
-        "common.genres"
-      );
-      t.strictEqual(
-        metadata.common.picture[0].format,
-        "image/jpeg",
-        "common.picture 0 format"
-      );
-      t.strictEqual(
-        metadata.common.picture[0].data.length,
-        80_938,
-        "common.picture 0 length"
-      );
-      t.strictEqual(
-        metadata.common.picture[1].format,
-        "image/jpeg",
-        "common.picture 1 format"
-      );
-      t.strictEqual(
-        metadata.common.picture[1].data.length,
-        80_938,
-        "common.picture 1 length"
-      );
-    });
+  expect(common.title, "title").toBe("Home");
+  expect(common.artist, "common.artist").toBe("Explo");
+  expect(common.artists, "common.artists").toStrictEqual([
+    "Explo",
+    "ions",
+    "nodejsftws",
+  ]);
+  expect(common.albumartist, "albumartist").toBe("Soundtrack");
+  expect(common.album, "album").toBe(
+    "Friday Night Lights [Original Movie Soundtrack]"
+  );
+  expect(common.year, "year").toBe(2004);
+  expect(common.track, "common.track").toStrictEqual({ no: 5, of: null });
+  expect(common.disk, "common.disk").toStrictEqual({ no: 1, of: 1 });
+  expect(common.genre, "common.genres").toStrictEqual(["Soundtrack", "OST"]);
+  expect(common.picture[0].format, "common.picture 0 format").toBe(
+    "image/jpeg"
+  );
+  expect(common.picture[0].data.length, "common.picture 0 length").toBe(80_938);
+  expect(common.picture[1].format, "common.picture 1 format").toBe(
+    "image/jpeg"
+  );
+  expect(common.picture[1].data.length, "common.picture 1 length").toBe(80_938);
+});
+
+// Issue: https://github.com/Borewit/music-metadata/issues/502
+test("COMM mapping", async () => {
+  const filePath = join(samplePath, "mp3", "issue-502.mp3");
+  const { common } = await parseFile(filePath);
+  expect(common.comment, "common.comment").toStrictEqual(["CLEAN"]);
+});
+
+test("should respect skipCovers-flag", async () => {
+  const filename = "id3v2.4.mp3";
+  const filePath = join(samplePath, filename);
+
+  const result = await parseFile(filePath, {
+    duration: true,
+    skipCovers: true,
   });
+  expect(
+    result.common.picture,
+    "common.picture should be undefined"
+  ).toBeUndefined();
+});
 
-  // Issue: https://github.com/Borewit/music-metadata/issues/502
-  it("COMM mapping", async () => {
-    const filePath = path.join(samplePath, "mp3", "issue-502.mp3");
-    const { common } = await mm.parseFile(filePath);
-    t.deepEqual(common.comment, ["CLEAN"], "common.comment");
-  });
+test("Map TXXX:ISRC", async () => {
+  const filename = "issue-802.mp3";
+  const filePath = join(samplePath, "mp3", filename);
 
-  it("should respect skipCovers-flag", () => {
-    const filename = "id3v2.4.mp3";
-    const filePath = path.join(samplePath, filename);
-
-    return mm
-      .parseFile(filePath, { duration: true, skipCovers: true })
-      .then((result) => {
-        t.isUndefined(
-          result.common.picture,
-          "common.picture should be undefined"
-        );
-      });
-  });
-
-  it("Map TXXX:ISRC", async () => {
-    const filename = "issue-802.mp3";
-    const filePath = path.join(samplePath, "mp3", filename);
-
-    const { common, native } = await mm.parseFile(filePath);
-    const id3v24 = native["ID3v2.4"];
-    t.isDefined(id3v24, "ID3v2.4 presence");
-    t.strictEqual(
-      id3v24.filter((tag) => {
-        return tag.id === "TSRC";
-      }).length,
-      0,
-      "ID3v2.4 tag TSRC not defined"
-    );
-    t.strictEqual(
-      id3v24.filter((tag) => {
-        return tag.id === "TXXX:ISRC";
-      }).length,
-      1,
-      "ID3v2.4 tag TXXX:ISRC to be defined"
-    );
-    t.includeDeepMembers(common.isrc, ["DEAE61300058"], "ISRC");
-  });
+  const { common, native } = await parseFile(filePath);
+  const id3v24 = native["ID3v2.4"];
+  expect(id3v24, "ID3v2.4 presence").toBeDefined();
+  expect(
+    id3v24.filter((tag) => tag.id === "TSRC"),
+    "ID3v2.4 tag TSRC not defined"
+  ).toHaveLength(0);
+  expect(
+    id3v24.filter((tag) => tag.id === "TXXX:ISRC"),
+    "ID3v2.4 tag TXXX:ISRC to be defined"
+  ).toHaveLength(1);
+  expect(common.isrc, "ISRC").toContain("DEAE61300058");
 });
