275c5b535e4c5260cd001294e291aad335eb220c
mikiymk <mikiymk001@gmail.com>
2022-07-08 16:36:54 +0900

    strtok3/read-tokenを更新

--- test/strtok3/test.ts
+++ test/strtok3/test.ts
@@ -108,48 +108,6 @@ async function peekOnData(tokenizer: ITokenizer): Promise<void> {
 
 for (const tokenizerType of tokenizerTests) {
   describe(tokenizerType.name, () => {
-    test("should decode buffer", async () => {
-      const rst = await getTokenizerWithData("\u0005peter", tokenizerType);
-      // should decode UINT8 from chunk
-      expect(rst.position).toBe(0);
-      let value: string | number = await rst.readToken(UINT8);
-      expect(typeof value).toBe("number");
-      expect(value, "0x05 == 5").toBe(5);
-      // should decode string from chunk
-      expect(rst.position).toBe(1);
-      value = await rst.readToken(new StringType(5, "utf8"));
-      expect(typeof value).toBe("string");
-      expect(value).toBe("peter");
-      expect(rst.position).toBe(6);
-      // should should reject at the end of the stream
-      try {
-        await rst.readToken(UINT8);
-        expect.fail("Should reject due to end-of-stream");
-      } catch (error) {
-        expect(error).toBeInstanceOf(EndOfStreamError);
-      }
-    });
-
-    test("should be able to read from an absolute offset", async () => {
-      const rst = await getTokenizerWithData("\u0005peter", tokenizerType);
-      // should decode UINT8 from chunk
-      expect(rst.position).toBe(0);
-      const value: string | number = await rst.readToken(
-        new StringType(5, "utf8"),
-        1
-      );
-      expect(typeof value).toBe("string");
-      expect(value).toBe("peter");
-      expect(rst.position).toBe(6);
-
-      try {
-        await rst.readToken(UINT8);
-        expect.fail("Should reject due to end-of-stream");
-      } catch (error) {
-        expect(error).toBeInstanceOf(EndOfStreamError);
-      }
-    });
-
     test("should pick length from buffer, if length is not explicit defined", async () => {
       const rst = await getTokenizerWithData("\u0005peter", tokenizerType);
 
