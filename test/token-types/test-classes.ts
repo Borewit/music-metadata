b4584cc947b826ac6c52fd97fe382d5ec0b1a7fc
mikiymk <mikiymk001@gmail.com>
2022-07-08 18:23:48 +0900

    token-types/test-classesを更新

--- test/token-types/test-classes.ts
+++ test/token-types/test-classes.ts
@@ -1,66 +1,57 @@
-import { describe, assert, it } from "vitest";
+import { describe, test, expect } from "vitest";
 import { BufferType, StringType, Uint8ArrayType } from "../../lib/token-types";
 
 describe("BufferType", () => {
-  it("Should copy data fom the source array", () => {
+  test("Should copy data fom the source array", () => {
     const source = Buffer.from([0xa1, 0xa2, 0xb1, 0xb2, 0xc1, 0xc2]);
 
     const bufferTypeToken = new BufferType(2);
     const bufferResult = bufferTypeToken.get(source, 2);
 
-    assert.deepStrictEqual(
-      bufferResult,
-      Buffer.from([0xb1, 0xb2]),
-      "should be 2 middle bytes: 0xb1, 0xb2"
+    expect(bufferResult, "should be 2 middle bytes: 0xb1, 0xb2").toEqual(
+      Buffer.from([0xb1, 0xb2])
     );
 
     // Overwrite the result
     bufferResult[0] = 0xd1;
     bufferResult[1] = 0xd2;
 
-    assert.notDeepEqual(
-      source,
-      Buffer.from([0xa1, 0xa2, 0xd1, 0xd2, 0xc1, 0xc2]),
-      "should copy the data"
+    expect(source, "should copy the data").not.toEqual(
+      Buffer.from([0xa1, 0xa2, 0xd1, 0xd2, 0xc1, 0xc2])
     );
   });
 });
 
 describe("Uint8ArrayType", () => {
-  it("should should not copy data fom the source array", () => {
+  test("should should not copy data fom the source array", () => {
     const source = Uint8Array.from([0xa1, 0xa2, 0xb1, 0xb2, 0xc1, 0xc2]);
 
     const uint8ArrayType = new Uint8ArrayType(2);
     const bufferResult = uint8ArrayType.get(source, 2);
 
-    assert.deepStrictEqual(
-      bufferResult,
-      Uint8Array.from([0xb1, 0xb2]),
-      "should be 2 middle bytes: 0xb1, 0xb2"
+    expect(bufferResult, "should be 2 middle bytes: 0xb1, 0xb2").toEqual(
+      Uint8Array.from([0xb1, 0xb2])
     );
 
     // Overwrite the result
     bufferResult[0] = 0xd1;
     bufferResult[1] = 0xd2;
 
-    assert.deepStrictEqual(
-      source,
-      Uint8Array.from([0xa1, 0xa2, 0xd1, 0xd2, 0xc1, 0xc2]),
-      "should not copy the data"
+    expect(source, "should not copy the data").toEqual(
+      Uint8Array.from([0xa1, 0xa2, 0xd1, 0xd2, 0xc1, 0xc2])
     );
   });
 });
 
 describe("StringType", () => {
-  it("decode from Buffer", () => {
+  test("decode from Buffer", () => {
     const source = new Uint8Array(Buffer.from("peter", "utf8"));
 
     const stringType = new StringType(5, "utf8");
 
-    assert.strictEqual(
+    expect(
       stringType.get(source, 0),
-      "peter",
       "should be 2 middle bytes: 0xb1, 0xb2"
-    );
+    ).toBe("peter");
   });
 });
