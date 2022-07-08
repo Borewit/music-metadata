e589c7b28850d47d8f67408b0e47892b83b5ed49
mikiymk <mikiymk001@gmail.com>
2022-07-08 17:24:21 +0900

    strtok3/ignoreを更新

---
+++ test/strtok3/ignore.test.ts
@@ -0,0 +1,63 @@
+import { describe, test, expect } from "vitest";
+import { EndOfStreamError } from "../../lib/strtok3";
+import { UINT32_LE, UINT32_BE, INT32_BE } from "../../lib/token-types";
+import { getTokenizerWithData, tokenizerCases } from "./util";
+
+const bufIncr5 = Buffer.from("\u0001\u0002\u0003\u0004\u0005", "ascii");
+const buf1A00 = Buffer.from([
+  0x1a, 0x00, 0x1a, 0x00, 0x1a, 0x00, 0x1a, 0x00, 0x1a, 0x00, 0x1a, 0x00, 0x1a,
+  0x00, 0x1a, 0x00,
+]);
+
+describe.each(tokenizerCases)("tokenizer from %s", (_name, load) => {
+  test("number", async () => {
+    const tokenizer = await getTokenizerWithData("increment-5", bufIncr5, load);
+    expect(tokenizer.fileInfo, "tokenizer.fileInfo").toBeDefined();
+    await tokenizer.ignore(tokenizer.fileInfo.size - 4);
+    const x = await tokenizer.peekNumber(INT32_BE);
+    expect(x).toBe(33_752_069);
+  });
+
+  test("should throw an Error if we reach EOF while peeking a number", async () => {
+    const tokenizer = await getTokenizerWithData("increment-5", bufIncr5, load);
+    expect(tokenizer.fileInfo, "tokenizer.fileInfo").toBeDefined();
+    await tokenizer.ignore(tokenizer.fileInfo.size - 3);
+    try {
+      await tokenizer.peekNumber(INT32_BE);
+      expect.fail("Should throw Error: End-Of-File");
+    } catch (error) {
+      expect(error).toBeInstanceOf(EndOfStreamError);
+    }
+    await tokenizer.close();
+  });
+
+  test("should be able to handle multiple ignores", async () => {
+    const tokenizer = await getTokenizerWithData("1A00", buf1A00, load);
+    let value = await tokenizer.readToken(UINT32_LE);
+    expect(typeof value).toBe("number");
+    expect(value, "UINT24_LE #1").toBe(0x00_1a_00_1a);
+    await tokenizer.ignore(UINT32_BE.len);
+    await tokenizer.ignore(UINT32_LE.len);
+    value = await tokenizer.readToken(UINT32_BE);
+    expect(typeof value).toBe("number");
+    expect(value, "UINT32_BE #4").toBe(0x1a_00_1a_00);
+    await tokenizer.close();
+  });
+
+  test("should be able to ignore (skip)", async () => {
+    const tokenizer = await getTokenizerWithData("1A00", buf1A00, load);
+    expect(tokenizer.position).toBe(0);
+    await tokenizer.ignore(4);
+    expect(tokenizer.position).toBe(4);
+    let value = await tokenizer.readToken(UINT32_BE);
+    expect(typeof value).toBe("number");
+    expect(value, "UINT32_BE #2").toBe(0x1a_00_1a_00);
+    value = await tokenizer.readToken(UINT32_LE);
+    expect(typeof value).toBe("number");
+    expect(value, "UINT32_LE #3").toBe(0x00_1a_00_1a);
+    value = await tokenizer.readToken(UINT32_BE);
+    expect(typeof value).toBe("number");
+    expect(value, "UINT32_BE #4").toBe(0x1a_00_1a_00);
+    await tokenizer.close();
+  });
+});
--- test/strtok3/test.ts
+++ test/strtok3/test.ts
@@ -66,57 +66,6 @@ const tokenizerTests: ITokenizerTest[] = [
 
 for (const tokenizerType of tokenizerTests) {
   describe(tokenizerType.name, () => {
-    test("number", async () => {
-      const tokenizer = await tokenizerType.loadTokenizer("test3.dat");
-      assert.isDefined(tokenizer.fileInfo, "tokenizer.fileInfo");
-      await tokenizer.ignore(tokenizer.fileInfo.size - 4);
-      const x = await tokenizer.peekNumber(INT32_BE);
-      expect(x).toBe(33_752_069);
-    });
-
-    test("should throw an Error if we reach EOF while peeking a number", async () => {
-      const tokenizer = await tokenizerType.loadTokenizer("test3.dat");
-      assert.isDefined(tokenizer.fileInfo, "tokenizer.fileInfo");
-      await tokenizer.ignore(tokenizer.fileInfo.size - 3);
-      try {
-        await tokenizer.peekNumber(INT32_BE);
-        assert.fail("Should throw Error: End-Of-File");
-      } catch (error) {
-        assert.instanceOf(error, EndOfStreamError);
-      }
-      await tokenizer.close();
-    });
-
-    test("should be able to handle multiple ignores", async () => {
-      const tokenizer = await tokenizerType.loadTokenizer("test1.dat");
-      let value = await tokenizer.readToken(UINT32_LE);
-      expect(typeof value).toBe("number");
-      expect(value, "UINT24_LE #1").toBe(0x00_1a_00_1a);
-      await tokenizer.ignore(UINT32_BE.len);
-      await tokenizer.ignore(UINT32_LE.len);
-      value = await tokenizer.readToken(UINT32_BE);
-      expect(typeof value).toBe("number");
-      expect(value, "UINT32_BE #4").toBe(0x1a_00_1a_00);
-      await tokenizer.close();
-    });
-
-    test("should be able to ignore (skip)", async () => {
-      const tokenizer = await tokenizerType.loadTokenizer("test1.dat");
-      expect(tokenizer.position).toBe(0);
-      await tokenizer.ignore(4);
-      expect(tokenizer.position).toBe(4);
-      let value = await tokenizer.readToken(UINT32_BE);
-      expect(typeof value).toBe("number");
-      expect(value, "UINT32_BE #2").toBe(0x1a_00_1a_00);
-      value = await tokenizer.readToken(UINT32_LE);
-      expect(typeof value).toBe("number");
-      expect(value, "UINT32_LE #3").toBe(0x00_1a_00_1a);
-      value = await tokenizer.readToken(UINT32_BE);
-      expect(typeof value).toBe("number");
-      expect(value, "UINT32_BE #4").toBe(0x1a_00_1a_00);
-      await tokenizer.close();
-    });
-
     describe("End-Of-File exception behaviour", () => {
       test("should not throw an Error if we read exactly until the end of the file", async () => {
         const rst = await getTokenizerWithData(
