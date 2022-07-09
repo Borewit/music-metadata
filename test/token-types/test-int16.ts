a1bf017c0e0401ca57ab91f802067ffb2ca93a2b
mikiymk <mikiymk001@gmail.com>
2022-07-09 21:38:45 +0900

    token-types/test-int16を更新

--- test/token-types/test-int16.ts
+++ test/token-types/test-int16.ts
@@ -1,61 +1,55 @@
 // Test reading int16 values.
 
-import { describe, assert, it } from "vitest";
-import * as Token from "../../lib/token-types";
-import * as util from "./util";
+import { describe, test, expect } from "vitest";
+import { INT16_BE, INT16_LE } from "../../lib/token-types";
+import { checkBuffer } from "./util";
 
 describe("Parse 16-bit signed integer", () => {
   describe("big-endian", () => {
-    it("should encode", () => {
+    test("should encode", () => {
       const buf = Buffer.alloc(2);
 
-      Token.INT16_BE.put(buf, 0, 0x00);
-      util.checkBuffer(buf, "0000");
+      INT16_BE.put(buf, 0, 0x00_00);
+      checkBuffer(buf, "0000");
 
-      Token.INT16_BE.put(buf, 0, 0x0f_0b);
-      util.checkBuffer(buf, "0f0b");
+      INT16_BE.put(buf, 0, 0x0f_0b);
+      checkBuffer(buf, "0f0b");
 
-      Token.INT16_BE.put(buf, 0, -0x0f_0b);
-      util.checkBuffer(buf, "f0f5");
+      INT16_BE.put(buf, 0, -0x0f_0b);
+      checkBuffer(buf, "f0f5");
     });
 
-    it("should decode", () => {
-      const buf = Buffer.from(
-        "\u000A\u001A\u0000\u0000\u00FF\u00FF\u0080\u0000",
-        "binary"
-      );
+    test("should decode", () => {
+      const buf = Buffer.from([0x0a, 0x1a, 0x00, 0x00, 0xff, 0xff, 0x80, 0x00]);
 
-      assert.equal(Token.INT16_BE.get(buf, 0), 2586);
-      assert.equal(Token.INT16_BE.get(buf, 2), 0);
-      assert.equal(Token.INT16_BE.get(buf, 4), -1);
-      assert.equal(Token.INT16_BE.get(buf, 6), -32_768);
+      expect(INT16_BE.get(buf, 0)).toBe(2586);
+      expect(INT16_BE.get(buf, 2)).toBe(0);
+      expect(INT16_BE.get(buf, 4)).toBe(-1);
+      expect(INT16_BE.get(buf, 6)).toBe(-32_768);
     });
   });
 
   describe("little-endian", () => {
-    it("should encode", () => {
+    test("should encode", () => {
       const buf = Buffer.alloc(2);
 
-      Token.INT16_LE.put(buf, 0, 0x00);
-      util.checkBuffer(buf, "0000");
+      INT16_LE.put(buf, 0, 0x00_00);
+      checkBuffer(buf, "0000");
 
-      Token.INT16_LE.put(buf, 0, 0x0f_0b);
-      util.checkBuffer(buf, "0b0f");
+      INT16_LE.put(buf, 0, 0x0f_0b);
+      checkBuffer(buf, "0b0f");
 
-      Token.INT16_LE.put(buf, 0, -0x0f_0b);
-      util.checkBuffer(buf, "f5f0");
+      INT16_LE.put(buf, 0, -0x0f_0b);
+      checkBuffer(buf, "f5f0");
     });
 
-    it("should decode", () => {
-      const buf = Buffer.from(
-        "\u001A\u000A\u0000\u0000\u00FF\u00FF\u0000\u0080",
-        "binary"
-      );
+    test("should decode", () => {
+      const buf = Buffer.from([0x1a, 0x0a, 0x00, 0x00, 0xff, 0xff, 0x00, 0x80]);
 
-      assert.equal(Token.INT16_LE.get(buf, 0), 2586);
-      assert.equal(Token.INT16_LE.get(buf, 2), 0);
-      assert.equal(Token.INT16_LE.get(buf, 4), -1);
-      assert.equal(Token.INT16_LE.get(buf, 6), -32_768);
+      expect(INT16_LE.get(buf, 0)).toBe(2586);
+      expect(INT16_LE.get(buf, 2)).toBe(0);
+      expect(INT16_LE.get(buf, 4)).toBe(-1);
+      expect(INT16_LE.get(buf, 6)).toBe(-32_768);
     });
   });
 });
