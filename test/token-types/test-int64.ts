5a8ce2812b182ae45164aec965ee4972c0c60fbd
mikiymk <mikiymk001@gmail.com>
2022-07-09 21:58:59 +0900

    token-types/test-int64を更新

--- test/token-types/test-int64.ts
+++ test/token-types/test-int64.ts
@@ -1,83 +1,69 @@
 // Test reading int64 values.
 
-import { describe, assert, it } from "vitest";
-import * as Token from "../../lib/token-types";
-import * as util from "./util";
+import { describe, test, expect } from "vitest";
+import { INT64_BE, INT64_LE } from "../../lib/token-types";
+import { checkBuffer } from "./util";
 
 describe("Parse 64-bit signed integer", () => {
   describe("big-endian", () => {
-    it("should encode", () => {
+    test("should encode", () => {
       const buf = Buffer.alloc(8);
 
-      Token.INT64_BE.put(buf, 0, BigInt(0x01));
-      util.checkBuffer(buf, "0000000000000001");
+      INT64_BE.put(buf, 0, 0x00_00_00_00_00_00_00_01n);
+      checkBuffer(buf, "0000000000000001");
 
-      Token.INT64_BE.put(buf, 0, BigInt(0x00_00_ff_bb_ee_dd_cc_aa));
-      util.checkBuffer(buf, "0000ffbbeeddccaa");
+      INT64_BE.put(buf, 0, 0x00_00_ff_bb_ee_dd_cc_aan);
+      checkBuffer(buf, "0000ffbbeeddccaa");
 
-      Token.INT64_BE.put(buf, 0, BigInt(-1));
-      util.checkBuffer(buf, "ffffffffffffffff");
+      INT64_BE.put(buf, 0, -1n);
+      checkBuffer(buf, "ffffffffffffffff");
     });
 
-    it("should decode", () => {
-      assert.strictEqual(
-        Token.INT64_BE.get(
-          Buffer.from(
-            "\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000",
-            "binary"
-          ),
-          0
-        ),
-        BigInt(0)
-      );
-      assert.strictEqual(
-        Token.INT64_BE.get(
-          Buffer.from(
-            "\u00FF\u00FF\u00FF\u00FF\u00FF\u00FF\u00FF\u00FF",
-            "binary"
-          ),
-          0
-        ),
-        BigInt(-1)
-      );
+    test("should decode", () => {
+      let buf: Buffer;
+
+      buf = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+      expect(INT64_BE.get(buf, 0)).toBe(0n);
+
+      buf = Buffer.from([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+      expect(INT64_BE.get(buf, 0)).toBe(-1n);
+
+      buf = Buffer.from([0x00, 0x00, 0xff, 0xbb, 0xee, 0xdd, 0xcc, 0xaa]);
+      expect(INT64_BE.get(buf, 0)).toBe(0x00_00_ff_bb_ee_dd_cc_aan);
+
+      buf = Buffer.from([0x00, 0x00, 0xff, 0xbb, 0xee, 0xdd, 0xcc, 0xbb]);
+      expect(INT64_BE.get(buf, 0)).toBe(0x00_00_ff_bb_ee_dd_cc_bbn);
     });
   });
 
   describe("little-endian", () => {
-    it("should encode", () => {
+    test("should encode", () => {
       const buf = Buffer.alloc(8);
 
-      Token.INT64_LE.put(buf, 0, BigInt(0x00));
-      util.checkBuffer(buf, "0000000000000000");
+      INT64_LE.put(buf, 0, 0x00_00_00_00_00_00_00_00n);
+      checkBuffer(buf, "0000000000000000");
 
-      Token.INT64_LE.put(buf, 0, BigInt(0x00_00_ff_bb_ee_dd_cc_aa));
-      util.checkBuffer(buf, "aaccddeebbff0000");
+      INT64_LE.put(buf, 0, 0x00_00_ff_bb_ee_dd_cc_aan);
+      checkBuffer(buf, "aaccddeebbff0000");
 
-      Token.INT64_LE.put(buf, 0, BigInt(-1));
-      util.checkBuffer(buf, "ffffffffffffffff");
+      INT64_LE.put(buf, 0, -1n);
+      checkBuffer(buf, "ffffffffffffffff");
     });
 
-    it("should decode", () => {
-      let buf = Buffer.from(
-        "\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u00FF\u00FF\u00FF\u00FF\u00FF\u00FF\u00FF\u00FF",
-        "binary"
-      );
+    test("should decode", () => {
+      let buf: Buffer;
 
-      assert.strictEqual(Token.INT64_LE.get(buf, 0), BigInt(0));
-      assert.strictEqual(Token.INT64_LE.get(buf, 8), BigInt(-1));
+      buf = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+      expect(INT64_LE.get(buf, 0)).toBe(0n);
 
-      buf = Buffer.from(
-        "\u00AA\u00CC\u00DD\u00EE\u00BB\u00FF\u0000\u0000\u00BB\u00CC\u00DD\u00EE\u00BB\u00FF\u0000\u0000",
-        "binary"
-      );
-      assert.strictEqual(
-        Token.INT64_LE.get(buf, 0),
-        BigInt(0x00_00_ff_bb_ee_dd_cc_aa)
-      );
-      assert.strictEqual(
-        Token.INT64_LE.get(buf, 8),
-        BigInt(0x00_00_ff_bb_ee_dd_cc_bb)
-      );
+      buf = Buffer.from([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+      expect(INT64_LE.get(buf, 0)).toBe(-1n);
+
+      buf = Buffer.from([0xaa, 0xcc, 0xdd, 0xee, 0xbb, 0xff, 0x00, 0x00]);
+      expect(INT64_LE.get(buf, 0)).toBe(0x00_00_ff_bb_ee_dd_cc_aan);
+
+      buf = Buffer.from([0xbb, 0xcc, 0xdd, 0xee, 0xbb, 0xff, 0x00, 0x00]);
+      expect(INT64_LE.get(buf, 0)).toBe(0x00_00_ff_bb_ee_dd_cc_bbn);
     });
   });
 });
