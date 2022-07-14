ed3047edfb0a9e470a3d87fcb4a8351c96b9f392
mikiymk <mikiymk001@gmail.com>
2022-07-14 23:46:05 +0900

    aiffを更新

--- lib/aiff/AiffTokenCommon.ts
+++ lib/aiff/AiffTokenCommon.ts
@@ -28,22 +28,22 @@ export class Common implements IGetToken<ICommon> {
     this.len = header.chunkSize;
   }
 
-  public get(buf: Buffer, off: number): ICommon {
+  public get(buf: Uint8Array, off: number): ICommon {
     // see: https://cycling74.com/forums/aiffs-80-bit-sample-rate-value
-    const shift = buf.readUInt16BE(off + 8) - 16_398;
-    const baseSampleRate = buf.readUInt16BE(off + 8 + 2);
+    const shift = Token.UINT16_BE.get(buf, off + 8) - 16_398;
+    const baseSampleRate = Token.UINT16_BE.get(buf, off + 8 + 2);
 
     const res: ICommon = {
-      numChannels: buf.readUInt16BE(off),
-      numSampleFrames: buf.readUInt32BE(off + 2),
-      sampleSize: buf.readUInt16BE(off + 6),
+      numChannels: Token.UINT16_BE.get(buf, off),
+      numSampleFrames: Token.UINT32_BE.get(buf, off + 2),
+      sampleSize: Token.UINT16_BE.get(buf, off + 6),
       sampleRate: shift < 0 ? baseSampleRate >> Math.abs(shift) : baseSampleRate << shift,
     };
 
     if (this.isAifc) {
       res.compressionType = FourCcToken.get(buf, off + 18);
       if (this.len > 22) {
-        const strLen = buf.readInt8(off + 22);
+        const strLen = Token.INT8.get(buf, off + 22);
         const padding = (strLen + 1) % 2;
         if (23 + strLen + padding === this.len) {
           res.compressionName = new Token.StringType(strLen, "binary").get(buf, off + 23);
