51a7738c6dbed49e2ad55c615290753dd64a2a19
mikiymk <mikiymk001@gmail.com>
2022-09-23 21:55:14 +0900

    パッケージの更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  build: {
-    outDir: "./vite-out",
-  },
-  test: {
-    include: [
-      "test/test-*.ts",
-      "test/token-types/test-*.ts",
-      "test/strtok3/test.ts",
-      "test/peek-readable/test-*.ts",
-      "test/file-type/test.ts",
-      "test/**/*.test.ts",
-      "lib/**/*.test.ts",
-      "packages/**/*.test.ts",
-    ],
-    exclude: ["test/test-*.d.ts"],
-    environment: "jsdom",
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "./vite-out",
+    },
+    test: {
+      include: [
+        "test/test-*.ts",
+        "test/token-types/test-*.ts",
+        "test/strtok3/test.ts",
+        "test/peek-readable/test-*.ts",
+        "test/file-type/test.ts",
+        "test/**/*.test.ts",
+        "lib/**/*.test.ts",
+        "packages/**/*.test.ts",
+      ],
+      exclude: ["test/test-*.d.ts"],
+      environment: "jsdom",
 
-    isolate: false,
-    reporters: ["verbose"],
-  },
+      isolate: false,
+      threads: false,
+      reporters: ["verbose"],
+    },
+  };
 });
