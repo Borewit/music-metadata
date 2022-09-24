c64706711f39c2b427ec0df5e42768cd9ba75d0f
mikiymk <mikiymk001@gmail.com>
2022-09-24 16:26:13 +0900

    投げられたエラーを戻り値に変換する関数

--- lib/parse-unit/primitive/integer.ts
+++ lib/parse-unit/primitive/integer.ts
@@ -1,16 +1,11 @@
+import { wrapError } from "../utility/wrap-error";
+
 import type { Unit } from "../type/unit";
 
 export const u8: Unit<number, RangeError> = [
   1,
   (buffer, offset) => {
     const dataview = new DataView(buffer.buffer);
-    try {
-      return dataview.getUint8(offset);
-    } catch (error) {
-      if (error instanceof RangeError) {
-        return error;
-      }
-      throw error;
-    }
+    return wrapError(() => dataview.getUint8(offset));
   },
 ];
