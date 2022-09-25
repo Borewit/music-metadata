dea19fa9960823688f984b83280cb38346429620
mikiymk <mikiymk001@gmail.com>
2022-09-25 23:49:44 +0900

    オブジェクトにマッピングするUnitを変更する

--- lib/parse-unit/combinate/__tests__/sequence-to-object.test.ts
+++ lib/parse-unit/combinate/__tests__/sequence-to-object.test.ts
@@ -5,54 +5,12 @@ import { u8 } from "../../primitive/integer";
 import { skip } from "../../primitive/skip";
 import { latin1 } from "../../primitive/string";
 import { readUnitFromTokenizer } from "../../utility/read-unit";
-import { seq2obj, sequenceToObject } from "../sequence-to-object";
+import { sequenceToObject } from "../sequence-to-object";
 
 const buffer = new Uint8Array([
   0x00, 0x01, 0x7f, 0x80, 0xff, 0x81, 0xe6, 0x47, 0x80, 0x6a, 0x5e, 0x4b, 0x2e, 0x63, 0x5a, 0x6d, 0xdd, 0x1d, 0xee,
   0x4e, 0xcd, 0x5f, 0x98, 0xb8,
 ]);
-
-test("unit: units sequence", async () => {
-  const tokenizer = new BufferTokenizer(buffer);
-
-  const result: Promise<{
-    first: number;
-    second: string;
-    third: undefined;
-    fourth: number;
-  }> = readUnitFromTokenizer(
-    tokenizer,
-    seq2obj(
-      ["first" as const, u8],
-      ["second" as const, latin1(2)],
-      ["third" as const, skip(3)],
-      ["fourth" as const, u8]
-    )
-  );
-  await expect(result).resolves.toEqual({ first: 0x00, second: "\u0001\u007F", third: undefined, fourth: 0xe6 });
-
-  const result2: Promise<{
-    first: number;
-    second: undefined;
-    third: string;
-    fourth: number;
-  }> = readUnitFromTokenizer(
-    tokenizer,
-    seq2obj(
-      ["first" as const, u8],
-      ["second" as const, skip(3)],
-      ["third" as const, latin1(5)],
-      ["fourth" as const, u8]
-    )
-  );
-
-  await expect(result2).resolves.toEqual({
-    first: 0x47,
-    second: undefined,
-    third: "\u004B\u002E\u0063\u005A\u006D",
-    fourth: 0xdd,
-  });
-});
 
 test("unit: units sequence", async () => {
   const tokenizer = new BufferTokenizer(buffer);
