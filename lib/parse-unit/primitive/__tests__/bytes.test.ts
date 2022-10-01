db198b8fadff8d5be688e3463633ad134c286c67
mikiymk <mikiymk001@gmail.com>
2022-10-02 01:14:06 +0900

    読み込んでBufferTokenizerを作るUnit

--- lib/parse-unit/primitive/__tests__/bytes.test.ts
+++ lib/parse-unit/primitive/__tests__/bytes.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "vitest";
 
 import { BufferTokenizer } from "../../../strtok3/BufferTokenizer";
 import { readUnitFromTokenizer } from "../../utility/read-unit";
-import { bytes } from "../bytes";
+import { bytes, bytesTokenizer } from "../bytes";
 
 const buffer = new Uint8Array([
   0x00, 0x01, 0x7f, 0x80, 0xff, 0x81, 0xe6, 0x47, 0x80, 0x6a, 0x5e, 0x4b, 0x2e, 0x63, 0x5a, 0x6d, 0xdd, 0x1d, 0xee,
@@ -18,4 +18,27 @@ test("unit: byte array", async () => {
   await expect(readUnitFromTokenizer(tokenizer, bytes(3))).resolves.toEqual(Uint8Array.of(0x6a, 0x5e, 0x4b));
   await expect(readUnitFromTokenizer(tokenizer, bytes(3))).resolves.toEqual(Uint8Array.of(0x2e, 0x63, 0x5a));
   await expect(readUnitFromTokenizer(tokenizer, bytes(3))).resolves.toEqual(Uint8Array.of(0x6d, 0xdd, 0x1d));
+});
+
+test("unit: buffer tokenizer", async () => {
+  const tokenizer = new BufferTokenizer(buffer);
+
+  await expect(readUnitFromTokenizer(tokenizer, bytesTokenizer(3))).resolves.toEqual(
+    new BufferTokenizer(Uint8Array.of(0x00, 0x01, 0x7f))
+  );
+  await expect(readUnitFromTokenizer(tokenizer, bytesTokenizer(3))).resolves.toEqual(
+    new BufferTokenizer(Uint8Array.of(0x80, 0xff, 0x81))
+  );
+  await expect(readUnitFromTokenizer(tokenizer, bytesTokenizer(3))).resolves.toEqual(
+    new BufferTokenizer(Uint8Array.of(0xe6, 0x47, 0x80))
+  );
+  await expect(readUnitFromTokenizer(tokenizer, bytesTokenizer(3))).resolves.toEqual(
+    new BufferTokenizer(Uint8Array.of(0x6a, 0x5e, 0x4b))
+  );
+  await expect(readUnitFromTokenizer(tokenizer, bytesTokenizer(3))).resolves.toEqual(
+    new BufferTokenizer(Uint8Array.of(0x2e, 0x63, 0x5a))
+  );
+  await expect(readUnitFromTokenizer(tokenizer, bytesTokenizer(3))).resolves.toEqual(
+    new BufferTokenizer(Uint8Array.of(0x6d, 0xdd, 0x1d))
+  );
 });
--- lib/parse-unit/primitive/bytes.ts
+++ lib/parse-unit/primitive/bytes.ts
@@ -1,6 +1,13 @@
+import { BufferTokenizer } from "../../strtok3/BufferTokenizer";
+
 import type { Unit } from "../type/unit";
 
 export const bytes = (length: number): Unit<Uint8Array, RangeError> => [
   length,
   (buffer, offset) => buffer.subarray(offset, offset + length),
+];
+
+export const bytesTokenizer = (length: number): Unit<BufferTokenizer, RangeError> => [
+  length,
+  (buffer, offset) => new BufferTokenizer(buffer.subarray(offset, offset + length)),
 ];
