9df4cf8af29e83b2effc3ecea68907ed4cf17518
Borewit <Borewit@users.noreply.github.com>
2024-06-29 17:33:56 +0200

    Fix decoding of numbers in AIFF-token

--- lib/aiff/AiffToken.ts
+++ lib/aiff/AiffToken.ts
@@ -33,20 +33,20 @@ export class Common implements IGetToken<ICommon> {
   public get(buf: Buffer, off: number): ICommon {
 
     // see: https://cycling74.com/forums/aiffs-80-bit-sample-rate-value
-    const shift = buf.readUInt16BE(off + 8) - 16398;
-    const baseSampleRate = buf.readUInt16BE(off + 8 + 2);
+    const shift = Token.UINT16_BE.get(buf, off + 8) - 16398;
+    const baseSampleRate = Token.UINT16_BE.get(buf, off + 8 + 2);
 
     const res: ICommon = {
-      numChannels: buf.readUInt16BE(off),
-      numSampleFrames: buf.readUInt32BE(off + 2),
-      sampleSize: buf.readUInt16BE(off + 6),
+      numChannels:  Token.UINT16_BE.get(buf, off),
+      numSampleFrames:  Token.UINT32_BE.get(buf, off + 2),
+      sampleSize:  Token.UINT16_BE.get(buf, off + 6),
       sampleRate: shift < 0 ? baseSampleRate >> Math.abs(shift) : baseSampleRate << shift
     };
 
     if (this.isAifc) {
       res.compressionType = FourCcToken.get(buf, off + 18);
       if (this.len > 22) {
-        const strLen = buf.readInt8(off + 22);
+        const strLen = Token.UINT8.get(buf, off + 22);
         if (strLen > 0) {
           const padding = (strLen + 1) % 2;
           if (23 + strLen + padding === this.len) {
