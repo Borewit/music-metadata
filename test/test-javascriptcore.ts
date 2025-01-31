96182180e340cea508fddc192adcd6bfc2f4613b
JonLuca DeCaro <jonluca.decaro@gmail.com>
2025-01-31 14:13:05 -0800

    chore: refactor tests to use generic parser logic

--- test/test-javascriptcore.ts
+++ test/test-javascriptcore.ts
@@ -1,23 +1,23 @@
 import { assert } from 'chai';
 import path from 'node:path';
 
-import * as mm from '../lib/index.js';
 import { samplePath } from './util.js';
-import fs from "node:fs/promises";
-
+import { Parsers } from "./metadata-parsers.js";
 
 describe('JavaScriptCore', () => {
 
   const mp3SamplePath = path.join(samplePath, 'mp3');
 
-  it('should parse blobs properly', async () => {
+  it('should parse mp3s correctly', async () => {
     const filePath = path.join(mp3SamplePath, 'lame-peak.mp3');
 
-    const data = await fs.readFile(filePath);
-    const blob = new Blob([data]);
-    const metadata = await mm.parseBlob(blob);
-
-    assert.isNotNull(metadata, 'metadata');
+    Parsers.forEach(parser => {
+      it(parser.description, async function(){
+        const { format, common } = await parser.initParser(() => this.skip(), filePath);
+        assert.isNotNull(format, 'format');
+        assert.isNotNull(common, 'common');
+      });
+    });
   });
 
 });
