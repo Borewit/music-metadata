65e6146668f8274c3956100e1c7f2ed3f301d245
Borewit <Borewit@users.noreply.github.com>
2025-09-26 13:06:06 +0200

    Add support Apple’s QuickTime File Format

--- lib/mp4/Mp4Loader.ts
+++ lib/mp4/Mp4Loader.ts
@@ -2,8 +2,8 @@ import type { IParserLoader } from '../ParserFactory.js';
 
 export const mp4ParserLoader: IParserLoader = {
   parserType: 'mp4',
-  extensions: ['.mp4', '.m4a', '.m4b', '.m4pa', 'm4v', 'm4r', '3gp'],
-  mimeTypes: ['audio/mp4', 'audio/m4a', 'video/m4v', 'video/mp4'],
+  extensions: ['.mp4', '.m4a', '.m4b', '.m4pa', 'm4v', 'm4r', '3gp', '.mov', '.movie', '.qt'],
+  mimeTypes: ['audio/mp4', 'audio/m4a', 'video/m4v', 'video/mp4', 'video/quicktime'],
   async load() {
     return (await import('./MP4Parser.js')).MP4Parser
   }
