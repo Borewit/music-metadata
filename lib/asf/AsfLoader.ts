922ffabad70844848d88315c942b132d23134188
Borewit <Borewit@users.noreply.github.com>
2026-03-11 17:32:47 +0100

    Map `.wma` and `.wmv.` extension to `AsfParser`

--- lib/asf/AsfLoader.ts
+++ lib/asf/AsfLoader.ts
@@ -2,7 +2,7 @@ import type { IParserLoader } from '../ParserFactory.js';
 
 export const asfParserLoader: IParserLoader = {
   parserType: 'asf',
-  extensions: ['.asf'],
+  extensions: ['.asf', '.wma', '.wmv'],
   mimeTypes: ['audio/ms-wma', 'video/ms-wmv', 'audio/ms-asf', 'video/ms-asf', 'application/vnd.ms-asf'],
   async load() {
     return (await import('./AsfParser.js')).AsfParser;
