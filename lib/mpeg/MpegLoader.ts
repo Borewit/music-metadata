dd2eefd1398a209808249604b68662eb9862dbf6
Borewit <Borewit@users.noreply.github.com>
2026-09-17 20:39:04 +0200

    Enable skipped unit test

--- lib/mpeg/MpegLoader.ts
+++ lib/mpeg/MpegLoader.ts
@@ -2,7 +2,7 @@ import type { IParserLoader } from '../ParserFactory.js';
 
 export const mpegParserLoader: IParserLoader = {
   parserType: 'mpeg',
-  extensions: ['.mp2', '.mp3', '.m2a', '.aac', 'aacp'],
+  extensions: ['.mp1', '.mp2', '.mp3', '.m2a', '.aac', 'aacp'],
   mimeTypes: ['audio/mpeg', 'audio/mp3', 'audio/aacs', 'audio/aacp'],
   async load() {
     return (await import('./MpegParser.js')).MpegParser;
